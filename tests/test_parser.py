37e25625f9415965fc040abf826ebf6e412022f6
Yifan Wu <tyivanwu@gmail.com>
2019-11-23 16:06:40 -0500

    feat: add parser

---
+++ tests/test_parser.py
@@ -0,0 +1,13 @@
+import pytest
+from puzzles.parser import parser
+
+
+def test_parser():
+    assert parser("3[abc]4[ab]c") == "abcabcabcababababc"
+    assert parser("10[a]") == "aaaaaaaaaa"
+    assert parser("2[3[a]b]") == "aaabaaab"
+    assert parser("0[abc]") == ""
+    assert (
+        parser("1[1[1[1[1[1[1[1[1[1[1[1[1[1[1[1[1[1[1[1[xx]]]]]]]]]]]]]]]]]]]]") == "xx"
+    )
+    assert(parser("a[]b") == "ab")

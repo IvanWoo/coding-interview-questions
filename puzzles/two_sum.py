6d58adf878cb65a7076fd479723a09a2c443b704
Yifan Wu <tyivanwu@gmail.com>
2019-11-23 13:21:41 -0500

    fix: update two_sum

--- puzzles/two_sum.py
+++ puzzles/two_sum.py
@@ -1,4 +1,7 @@
-def two_sum(nums, target):
+from typing import List
+
+
+def two_sum(nums: int, target: int) -> List[int]:
     """
     given an array of integers, return indices of the two numbers such that they add up to a specific target
     assume each input would have exactly one solution, and you may use the same element twice
@@ -6,13 +9,11 @@ def two_sum(nums, target):
     :param target:
     :return:
     """
-    for i in range(len(nums)):
-        left = target - nums[i]
-        try:
-            j = nums.index(left)
-            return [i, j]
-        except ValueError:
-            pass
+    hash_table = dict([(v, k) for k, v in enumerate(nums)])
+    for i, num in enumerate(nums):
+        left = target - num
+        if left in hash_table:
+            return [i, hash_table[left]]
 
 
 def two_sum_alt(nums, target):
@@ -28,7 +29,3 @@ def two_sum_alt(nums, target):
             if nums[i] + nums[j] == target:
                 return [i, j]
 
-
-if __name__ == '__main__':
-    print(two_sum([1, 2, 7, 11, 15], 9))
-    print(two_sum([1, 2, 7, 11, 15], 9) == [1, 2])

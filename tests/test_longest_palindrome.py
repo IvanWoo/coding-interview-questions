6d6e70aafdf1532143c9d6a7fc420c430e21e538
Yifan Wu <tyivanwu@gmail.com>
2019-12-01 11:13:51 -0500

    feat: add longest_palindrome

---
+++ tests/test_longest_palindrome.py
@@ -0,0 +1,8 @@
+import pytest
+from puzzles.longest_palindrome import longest_palindrome
+
+
+def test_longest_palindrome():
+    assert longest_palindrome("babad") == "bab"
+    assert longest_palindrome("cbbd") == "bb"
+    assert longest_palindrome("a") == "a"

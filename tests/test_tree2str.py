9dcdb430efb9c21ea3f9ef860bbc768c69dc0cf8
Yifan Wu <tyivanwu@gmail.com>
2020-07-24 12:19:48 -0400

    feat: add tree2str

---
+++ tests/test_tree2str.py
@@ -0,0 +1,16 @@
+import pytest
+from puzzles.utils import TreeNode
+from puzzles.tree2str import tree2str
+
+
+@pytest.fixture
+def tree_14():
+    ts = {i: TreeNode(i) for i in range(1, 5)}
+    ts[1].left = ts[2]
+    ts[1].right = ts[3]
+    ts[2].left = ts[4]
+    return ts
+
+
+def test_tree2str(tree_14):
+    assert tree2str(tree_14[1]) == "1(2(4))(3)"

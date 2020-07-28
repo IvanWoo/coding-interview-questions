3369bb9f032a8b2a0dac5223133355e5cedd724c
Yifan Wu <tyivanwu@gmail.com>
2020-07-28 12:08:41 -0400

    feat: add construct_binary_tree_from_inorder_and_postorder_traversal

---
+++ puzzles/construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+# Given inorder and postorder traversal of a tree, construct the binary tree.
+
+# Note:
+# You may assume that duplicates do not exist in the tree.
+
+# For example, given
+
+# inorder = [9,3,15,20,7]
+# postorder = [9,15,7,20,3]
+# Return the following binary tree:
+
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+
+from typing import List
+from puzzles.utils import TreeNode
+
+
+def build_tree(inorder: List[int], postorder: List[int]) -> TreeNode:
+    if (not inorder) or (not postorder) or (len(inorder) != len(postorder)):
+        return
+
+    def helper(inorder, postorder, in_st, in_end, post_end):
+        if in_st > in_end or post_end < 0:
+            return
+        root = TreeNode(postorder[post_end])
+        ind = inorder.index(postorder[post_end])
+        root.left = helper(
+            inorder, postorder, in_st, ind - 1, post_end - 1 - (in_end - ind)
+        )
+        root.right = helper(inorder, postorder, ind + 1, in_end, post_end - 1)
+        return root
+
+    return helper(inorder, postorder, 0, len(inorder) - 1, len(postorder) - 1)
---
+++ tests/test_construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -0,0 +1,21 @@
+import pytest
+from puzzles.utils import TreeNode
+from puzzles.construct_binary_tree_from_inorder_and_postorder_traversal import (
+    build_tree,
+)
+
+
+@pytest.fixture
+def tree_15():
+    ts = {i: TreeNode(i) for i in range(1, 6)}
+    ts[1].left = ts[2]
+    ts[1].right = ts[3]
+    ts[2].left = ts[4]
+    ts[2].right = ts[5]
+    return ts
+
+
+def test_build_tree(tree_15):
+    inorder = [4, 2, 5, 1, 3]
+    postorder = [4, 5, 2, 3, 1]
+    assert build_tree(inorder, postorder) == tree_15[1]

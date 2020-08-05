75c2657c0866d20a911f877cc0d59089920a62e1
Yifan Wu <tyivanwu@gmail.com>
2020-08-05 12:26:32 -0400

    feat: add count_number_of_teams

---
+++ tests/test_count_number_of_teams.py
@@ -0,0 +1,11 @@
+import pytest
+from puzzles.count_number_of_teams import num_teams
+
+
+def test_num_teams():
+    assert num_teams([2, 5, 3, 4, 1]) == 3
+    assert num_teams([2, 1, 3]) == 0
+    assert num_teams([1, 2, 3, 4]) == 4
+    assert num_teams([1, 2]) == 0
+    assert num_teams([]) == 0
+

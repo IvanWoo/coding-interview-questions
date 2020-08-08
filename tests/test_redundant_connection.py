14a4bf25ec535cbfc13dc8c2d4344f9d465d787e
Yifan Wu <tyivanwu@gmail.com>
2020-08-08 01:43:49 -0400

    feat: add redundant_connection

---
+++ tests/test_redundant_connection.py
@@ -0,0 +1,7 @@
+import pytest
+from puzzles.redundant_connection import find_redundant_connection
+
+
+def test_find_redundant_connection():
+    assert find_redundant_connection([[1, 2], [1, 3], [2, 3]]) == [2, 3]
+

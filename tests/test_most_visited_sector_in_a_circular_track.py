95ddb58d547f34ba1d818f7749dde52d6f6de1ed
Yifan Wu <tyivanwu@gmail.com>
2020-08-26 23:21:45 -0400

    feat: add most_visited_sector_in_a_circular_track

---
+++ tests/test_most_visited_sector_in_a_circular_track.py
@@ -0,0 +1,7 @@
+import pytest
+from puzzles.most_visited_sector_in_a_circular_track import most_visited
+
+
+def test_most_visited():
+    assert most_visited(4, [1, 3, 1, 2]) == [1, 2]
+    assert most_visited(3, [3, 2, 1, 2, 1, 3, 2, 1, 2, 1, 3, 2, 3, 1]) == [1, 3]

a84a323079128d55a848cb5cdbf6501d06d443f7
Yifan Wu <tyivanwu@gmail.com>
2020-10-04 14:41:44 -0400

    feat: add dp solution for interleaving_string

--- puzzles/interleaving_string.py
+++ puzzles/interleaving_string.py
@@ -47,3 +47,54 @@ def is_interleave(s1: str, s2: str, s3: str) -> bool:
                 return helper(i1, i2 + 1, i3 + 1)
 
     return helper(0, 0, 0)
+
+
+# space O(m*n)
+def is_interleave(s1: str, s2: str, s3: str) -> bool:
+    n1, n2, n3 = len(s1), len(s2), len(s3)
+    if n1 + n2 != n3:
+        return False
+
+    dp = [[None] * (n1 + 1) for _ in range(n2 + 1)]
+    dp[0][0] = True
+
+    # first row
+    for c in range(1, n1 + 1):
+        dp[0][c] = s1[c - 1] == s3[c - 1] and dp[0][c - 1]
+
+    # first col
+    for r in range(1, n2 + 1):
+        dp[r][0] = s2[r - 1] == s3[r - 1] and dp[r - 1][0]
+
+    for r in range(1, n2 + 1):
+        for c in range(1, n1 + 1):
+            dp[r][c] = (s3[r + c - 1] == s1[c - 1] and dp[r][c - 1]) or (
+                s3[r + c - 1] == s2[r - 1] and dp[r - 1][c]
+            )
+
+    return dp[-1][-1]
+
+
+# space O(m)
+def is_interleave(s1: str, s2: str, s3: str) -> bool:
+    n1, n2, n3 = len(s1), len(s2), len(s3)
+    if n1 + n2 != n3:
+        return False
+
+    dp = [None] * (n1 + 1)
+    dp[0] = True
+
+    # first row
+    for c in range(1, n1 + 1):
+        dp[c] = s1[c - 1] == s3[c - 1] and dp[c - 1]
+
+    for r in range(1, n2 + 1):
+        for c in range(n1 + 1):
+            if c == 0:
+                dp[c] = dp[c] and s3[r + c - 1] == s2[r - 1]
+            else:
+                dp[c] = (s3[r + c - 1] == s1[c - 1] and dp[c - 1]) or (
+                    s3[r + c - 1] == s2[r - 1] and dp[c]
+                )
+
+    return dp[-1]

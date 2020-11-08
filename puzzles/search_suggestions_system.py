584bbc7f3a607626313e6f8e2cdcf4cad2d3509f
Yifan Wu <tyivanwu@gmail.com>
2020-11-08 12:16:04 -0500

    feat: add search_suggestions_system

---
+++ puzzles/search_suggestions_system.py
@@ -0,0 +1,100 @@
+# https://leetcode.com/problems/search-suggestions-system/
+"""
+Given an array of strings products and a string searchWord. We want to design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with the searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
+
+Return list of lists of the suggested products after each character of searchWord is typed. 
+
+Example 1:
+Input: products = ["mobile","mouse","moneypot","monitor","mousepad"], searchWord = "mouse"
+Output: [
+["mobile","moneypot","monitor"],
+["mobile","moneypot","monitor"],
+["mouse","mousepad"],
+["mouse","mousepad"],
+["mouse","mousepad"]
+]
+Explanation: products sorted lexicographically = ["mobile","moneypot","monitor","mouse","mousepad"]
+After typing m and mo all products match and we show user ["mobile","moneypot","monitor"]
+After typing mou, mous and mouse the system suggests ["mouse","mousepad"]
+
+Example 2:
+Input: products = ["havana"], searchWord = "havana"
+Output: [["havana"],["havana"],["havana"],["havana"],["havana"],["havana"]]
+
+Example 3:
+Input: products = ["bags","baggage","banner","box","cloths"], searchWord = "bags"
+Output: [["baggage","bags","banner"],["baggage","bags","banner"],["baggage","bags"],["bags"]]
+
+Example 4:
+Input: products = ["havana"], searchWord = "tatiana"
+Output: [[],[],[],[],[],[],[]]
+ 
+Constraints:
+
+1 <= products.length <= 1000
+There are no repeated elements in products.
+1 <= Σ products[i].length <= 2 * 10^4
+All characters of products[i] are lower-case English letters.
+1 <= searchWord.length <= 1000
+All characters of searchWord are lower-case English letters.
+"""
+from dataclasses import dataclass, field
+from typing import Any, Dict, Optional, List
+
+
+@dataclass
+class TrieNode:
+    children: Dict[str, "TrieNode"] = field(default_factory=dict)
+    value: Optional[Any] = field(default_factory=list)
+
+
+class ProductDictionary:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+
+    def add(self, product: str) -> None:
+        node = self.root
+        for char in product:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+            node.value.append(product)
+        return
+
+    def search(self, word: str) -> List[str]:
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                return []
+        return sorted(node.value)[:3]
+
+
+def suggested_products(products: List[str], searchWord: str) -> List[List[str]]:
+    pd = ProductDictionary()
+    for product in products:
+        pd.add(product)
+
+    ans = []
+    n = len(searchWord)
+    for i in range(1, n + 1):
+        ans.append(pd.search(searchWord[:i]))
+    return ans
+
+
+from bisect import bisect_left
+
+
+def suggested_products(products: List[str], searchWord: str) -> List[List[str]]:
+    products.sort()
+    ans, prefix, i = [], "", 0
+    for char in searchWord:
+        prefix += char
+        i = bisect_left(products, prefix, i)
+        ans.append([w for w in products[i : i + 3] if w.startswith(prefix)])
+    return ans
+
+
+if __name__ == "__main__":
+    suggested_products(["mobile", "mouse", "moneypot", "monitor", "mousepad"], "mouse")

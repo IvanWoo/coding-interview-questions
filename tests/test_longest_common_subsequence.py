b0f44de3504c6fa659e2fcfdcb6ce7642fa14c7a
Yifan Wu <tyivanwu@gmail.com>
2020-11-29 10:49:12 -0500

    feat: add longest_common_subsequence

---
+++ tests/test_longest_common_subsequence.py
@@ -0,0 +1,8 @@
+from puzzles.longest_common_subsequence import longest_common_subsequence
+
+
+def test_longest_common_subsequence():
+    assert longest_common_subsequence(text1="abcde", text2="ace") == 3
+    assert longest_common_subsequence(text1="abc", text2="abc") == 3
+    assert longest_common_subsequence(text1="abc", text2="def") == 0
+    assert longest_common_subsequence(text1="bsbininm", text2="jmjkbkjkv") == 1

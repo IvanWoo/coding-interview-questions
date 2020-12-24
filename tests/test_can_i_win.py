bc6bb8565794aef42a47225e000f59806111424c
Yifan Wu <tyivanwu@gmail.com>
2020-12-24 09:38:31 -0500

    feat: add can_i_win

---
+++ tests/test_can_i_win.py
@@ -0,0 +1,9 @@
+from puzzles.can_i_win import can_i_win
+
+
+def test_can_i_win():
+    assert can_i_win(10, 11) == True
+    assert can_i_win(10, 0) == True
+    assert can_i_win(10, 1) == True
+    assert can_i_win(3, 6) == True
+    assert can_i_win(10, 40) == False

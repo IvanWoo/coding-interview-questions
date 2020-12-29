99ff29ade3a4988c3f73300f7ef165f6dd4d459b
Yifan Wu <tyivanwu@gmail.com>
2020-12-29 16:11:11 -0500

    chore: fix test

--- tests/test_can_i_win.py
+++ tests/test_can_i_win.py
@@ -2,7 +2,7 @@ from puzzles.can_i_win import can_i_win
 
 
 def test_can_i_win():
-    assert can_i_win(10, 11) == True
+    assert can_i_win(10, 11) == False
     assert can_i_win(10, 0) == True
     assert can_i_win(10, 1) == True
     assert can_i_win(3, 6) == True
--- tests/test_remove_invalid_parentheses.py
+++ tests/test_remove_invalid_parentheses.py
@@ -2,7 +2,7 @@ from puzzles.remove_invalid_parentheses import remove_invalid_parentheses
 
 
 def test_remove_invalid_parentheses():
-    assert remove_invalid_parentheses("()())()") == ["()()()", "(())()"]
+    assert sorted(remove_invalid_parentheses("()())()")) == sorted(["()()()", "(())()"])
     assert sorted(remove_invalid_parentheses("(a)())()")) == sorted(
         ["(a)()()", "(a())()"]
     )

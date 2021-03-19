0f4650ce515fe9c41a56ada1a0d3a919f65dc2a6
Yifan Wu <tyivanwu@gmail.com>
2021-03-18 21:33:19 -0400

    feat: add minimum_number_of_operations_to_move_all_balls_to_each_box

---
+++ tests/test_minimum_number_of_operations_to_move_all_balls_to_each_box.py
@@ -0,0 +1,8 @@
+from puzzles.minimum_number_of_operations_to_move_all_balls_to_each_box import (
+    min_operations,
+)
+
+
+def test_min_operations():
+    assert min_operations("110") == [1, 1, 3]
+    assert min_operations("001011") == [11, 8, 5, 4, 3, 4]

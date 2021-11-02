8fd90feb83cb7941c0c1152230cec66e3e5ab802
Yifan Wu <tyivanwu@gmail.com>
2021-11-01 22:10:11 -0400

    feat: add house_robber

---
+++ tests/test_house_robber_ii.py
@@ -0,0 +1,10 @@
+import pytest
+from puzzles.house_robber_ii import rob
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [([1], 1), ([2, 3, 2], 3), ([1, 2, 3, 1], 4), ([1, 2, 3], 3)],
+)
+def test_rob(nums, expected):
+    assert rob(nums) == expected

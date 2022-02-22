89ead56ab64e0bf7ae1d3e4a45f237b58c45f117
Yifan Wu <tyivanwu@gmail.com>
2022-02-22 18:58:24 -0500

    chore: fix tests syntax

--- tests/test_maximum_sum_circular_subarray.py
+++ tests/test_maximum_sum_circular_subarray.py
@@ -4,11 +4,13 @@ from puzzles.maximum_sum_circular_subarray import max_subarray_sum_circular
 
 @pytest.mark.parametrize(
     ("nums", "expected"),
-    ([1, -2, 3, -2], 3),
-    ([5, -3, 5], 10),
-    ([3, -1, 2, -1], 4),
-    ([3, -2, 2, -3], 3),
-    ([-2, -3, -1], -1),
+    [
+        ([1, -2, 3, -2], 3),
+        ([5, -3, 5], 10),
+        ([3, -1, 2, -1], 4),
+        ([3, -2, 2, -3], 3),
+        ([-2, -3, -1], -1),
+    ],
 )
 def test_max_subarray_sum_circular(nums, expected):
     assert max_subarray_sum_circular(nums) == expected

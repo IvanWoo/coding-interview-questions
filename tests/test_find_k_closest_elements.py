ce48678ce1afa7a7afae276963df0421ccb296f3
Yifan Wu <tyivanwu@gmail.com>
2022-03-27 22:42:45 -0400

    chore: refactor tests

--- tests/test_find_k_closest_elements.py
+++ tests/test_find_k_closest_elements.py
@@ -1,7 +1,14 @@
+import pytest
 from puzzles.find_k_closest_elements import find_closest_elements
 
 
-def test_find_closest_elements():
-    assert find_closest_elements(arr=[1, 2, 3, 4, 5], k=4, x=3) == [1, 2, 3, 4]
-    assert find_closest_elements(arr=[1, 2, 3, 4, 5], k=4, x=-1) == [1, 2, 3, 4]
-    assert find_closest_elements([1, 1, 1, 10, 10, 10], 1, 9) == [10]
+@pytest.mark.parametrize(
+    "arr, k, x, expected",
+    [
+        ([1, 2, 3, 4, 5], 4, 3, [1, 2, 3, 4]),
+        ([1, 2, 3, 4, 5], 4, -1, [1, 2, 3, 4]),
+        ([1, 1, 1, 10, 10, 10], 1, 9, [10]),
+    ],
+)
+def test_find_closest_elements(arr, k, x, expected):
+    assert find_closest_elements(arr, k, x) == expected

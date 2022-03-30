f44d3ef16f3783ed016ed8615fc70a9a03b7e7a9
Yifan Wu <tyivanwu@gmail.com>
2022-03-29 21:24:45 -0400

    feat: add script to generate scaffold

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+## tasks
+
+create a new puzzle
+
+```sh
+pipenv run new <puzzle name>
+```
+
 ## References
 
 - [TimeComplexity for Python](https://wiki.python.org/moin/TimeComplexity)
---
+++ scripts/new.py
@@ -0,0 +1,45 @@
+import argparse
+import pathlib
+
+SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
+ROOT_DIR = SCRIPT_DIR.parent.resolve()
+PUZZLES_DIR = ROOT_DIR / "puzzles"
+TESTS_DIR = ROOT_DIR / "tests"
+
+
+def _create_new_puzzle(name):
+    file_path = PUZZLES_DIR / f"{name}.py"
+    if file_path.exists():
+        print(f"Puzzle already exists: {name}")
+        return
+    print(f"Creating new puzzle: {name}")
+    with file_path.open("w") as f:
+        f.write("")
+
+
+def _create_new_test(name):
+    file_path = TESTS_DIR / f"test_{name}.py"
+    if file_path.exists():
+        print(f"Test already exists: {name}")
+        return
+    print(f"Creating new test: {name}")
+    with file_path.open("w") as f:
+        f.write(f"import pytest\nfrom puzzles.{name} import *\n\n")
+
+
+def create(name):
+    _create_new_puzzle(name)
+    _create_new_test(name)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Create a new puzzle")
+    parser.add_argument("name", help="The name of the new puzzle")
+    args = parser.parse_args()
+
+    name = args.name.lower().replace(" ", "_")
+    create(name)
+
+
+if __name__ == "__main__":
+    main()

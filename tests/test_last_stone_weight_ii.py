1c455a9128721b49e1b4960c6f076b1666fba45b
Yifan Wu <tyivanwu@gmail.com>
2022-04-06 22:48:04 -0400

    feat: add last_stone_weight_ii

---
+++ tests/test_last_stone_weight_ii.py
@@ -0,0 +1,10 @@
+import pytest
+from puzzles.last_stone_weight_ii import last_stone_weight_ii
+
+
+@pytest.mark.parametrize(
+    "stones, expected",
+    [([2, 7, 4, 1, 8, 1], 1), ([31, 26, 33, 21, 40], 5), ([1, 2], 1)],
+)
+def test_last_stone_weight_ii(stones, expected):
+    assert last_stone_weight_ii(stones) == expected

c2219b14769dc9f8333e34a3e919e6f3e02268d7
Yifan Wu <tyivanwu@gmail.com>
2022-04-07 23:22:29 -0400

    feat: add coin_change

---
+++ puzzles/coin_change.py
@@ -0,0 +1,60 @@
+# https://leetcode.com/problems/coin-change/
+"""
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+Example 1:
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+
+Example 2:
+Input: coins = [2], amount = 3
+Output: -1
+
+Example 3:
+Input: coins = [1], amount = 0
+Output: 0
+ 
+
+Constraints:
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+"""
+from functools import lru_cache
+
+
+def coin_change(coins: list[int], amount: int) -> int:
+    @lru_cache(None)
+    def helper(coins, amount):
+        if amount == 0:
+            return 0
+        if amount < 0:
+            return -1
+
+        res = float("inf")
+        for c in coins:
+            sub_question = helper(coins, amount - c)
+            if sub_question == -1:
+                continue
+            res = min(res, sub_question + 1)
+        return res if res != float("inf") else -1
+
+    return helper(tuple(coins), amount)
+
+
+def coin_change(coins: list[int], amount: int) -> int:
+    n = amount + 1
+    dp = [n] * n
+
+    dp[0] = 0
+    for i in range(n):
+        for coin in coins:
+            if i - coin < 0:
+                continue
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != n else -1
---
+++ tests/test_coin_change.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.coin_change import coin_change
+
+
+@pytest.mark.parametrize(
+    "coins, amount, expected",
+    [
+        ([1, 2, 5], 11, 3),
+        ([2], 3, -1),
+        ([1], 0, 0),
+    ],
+)
+def test_coin_change(coins, amount, expected):
+    assert coin_change(coins, amount) == expected

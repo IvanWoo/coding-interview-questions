bf54d146977befe0446d61af66a9f9175b48b622
Yifan Wu <tyivanwu@gmail.com>
2022-04-08 21:53:22 -0400

    feat: add top_k_frequent_elements

---
+++ puzzles/top_k_frequent_elements.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/top-k-frequent-elements/
+"""
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+ 
+Constraints:
+1 <= nums.length <= 105
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+ 
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+"""
+from collections import defaultdict, Counter
+
+
+def top_k_frequent(nums: list[int], k: int) -> list[int]:
+    counter = defaultdict(int)
+    for n in nums:
+        counter[n] += 1
+
+    sorted_q = sorted([(val, key) for key, val in counter.items()], reverse=True)
+    return [key for _, key in sorted_q[0:k]]
+
+
+def top_k_frequent(nums: list[int], k: int) -> list[int]:
+    counter = Counter(nums)
+    return [key for key, _ in counter.most_common(k)]
---
+++ tests/test_top_k_frequent_elements.py
@@ -0,0 +1,13 @@
+import pytest
+from puzzles.top_k_frequent_elements import top_k_frequent
+
+
+@pytest.mark.parametrize(
+    "nums, k, expected",
+    [
+        ([1, 1, 1, 2, 2, 3], 2, [1, 2]),
+        ([1], 1, [1]),
+    ],
+)
+def test_top_k_frequent(nums, k, expected):
+    assert top_k_frequent(nums, k) == expected

6b7ba4acb67ab6eeb7a9675b192947cfdda3be95
Yifan Wu <tyivanwu@gmail.com>
2022-04-11 19:11:50 -0400

    feat: add unique_email_addresses

---
+++ tests/test_unique_email_addresses.py
@@ -0,0 +1,20 @@
+import pytest
+from puzzles.unique_email_addresses import num_unique_emails
+
+
+@pytest.mark.parametrize(
+    "emails, expected",
+    [
+        (
+            [
+                "test.email+alex@leetcode.com",
+                "test.e.mail+bob.cathy@leetcode.com",
+                "testemail+david@lee.tcode.com",
+            ],
+            2,
+        ),
+        (["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"], 3),
+    ],
+)
+def test_num_unique_emails(emails, expected):
+    assert num_unique_emails(emails) == expected

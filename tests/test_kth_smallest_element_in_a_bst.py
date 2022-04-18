f3fd47da36d37e6d1259fcb02d44e56e1f805f8a
Yifan Wu <tyivanwu@gmail.com>
2022-04-17 20:44:18 -0400

    feat: add kth_smallest_element_in_a_bst

---
+++ tests/test_kth_smallest_element_in_a_bst.py
@@ -0,0 +1,24 @@
+import pytest
+from puzzles.utils import TreeNode
+from puzzles.kth_smallest_element_in_a_bst import kth_smallest
+
+
+@pytest.mark.parametrize(
+    "root, k, expected",
+    [
+        (
+            TreeNode(3, TreeNode(1), TreeNode(4, TreeNode(2))),
+            1,
+            1,
+        ),
+        (
+            TreeNode(
+                5, TreeNode(3, TreeNode(2, TreeNode(1), None), TreeNode(4)), TreeNode(6)
+            ),
+            3,
+            3,
+        ),
+    ],
+)
+def test(root, k, expected):
+    assert kth_smallest(root, k) == expected

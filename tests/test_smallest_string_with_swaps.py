6e593bbe89287ae2814b84a4bc0859a24e1108db
Yifan Wu <tyivanwu@gmail.com>
2022-04-26 21:46:21 -0400

    refactor: test_smallest_string_with_swaps

--- tests/test_smallest_string_with_swaps.py
+++ tests/test_smallest_string_with_swaps.py
@@ -1,9 +1,14 @@
+import pytest
 from puzzles.smallest_string_with_swaps import smallest_string_with_swaps
 
 
-def test_smallest_string_with_swaps():
-    assert smallest_string_with_swaps(s="dcab", pairs=[[0, 3], [1, 2]]) == "bacd"
-    assert (
-        smallest_string_with_swaps(s="dcab", pairs=[[0, 3], [1, 2], [0, 2]]) == "abcd"
-    )
-    assert smallest_string_with_swaps(s="cba", pairs=[[0, 1], [1, 2]]) == "abc"
+@pytest.mark.parametrize(
+    "s, pairs, expected",
+    [
+        ("dcab", [[0, 3], [1, 2]], "bacd"),
+        ("dcab", [[0, 3], [1, 2], [0, 2]], "abcd"),
+        ("cba", [[0, 1], [1, 2]], "abc"),
+    ],
+)
+def test_smallest_string_with_swaps(s, pairs, expected):
+    assert smallest_string_with_swaps(s, pairs) == expected

c3b46a372d1836c4a9d999f2708bbc32775bb9d6
Yifan Wu <tyivanwu@gmail.com>
2022-04-28 21:42:20 -0400

    feat: add is_graph_bipartite

---
+++ tests/test_is_graph_bipartite.py
@@ -0,0 +1,16 @@
+import pytest
+from puzzles.is_graph_bipartite import is_bipartite
+
+
+@pytest.mark.parametrize(
+    "graph, expected",
+    [
+        ([[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]], False),
+        ([[1, 3], [0, 2], [1, 3], [0, 2]], True),
+        ([[1], [0]], True),
+        ([[1], [0], []], True),
+        ([[1, 2, 3], [0], [0], [0]], True),
+    ],
+)
+def test_is_bipartite(graph, expected):
+    assert is_bipartite(graph) == expected

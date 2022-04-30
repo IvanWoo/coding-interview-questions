e98e350e04b3476578fdb64c428d6199a4df6914
Yifan Wu <tyivanwu@gmail.com>
2022-04-29 23:45:26 -0400

    refactor: update test_evaluate_division

--- tests/test_evaluate_division.py
+++ tests/test_evaluate_division.py
@@ -1,28 +1,29 @@
+import pytest
 from puzzles.evaluate_division import calc_equation
 
 
-def test_calc_equation():
-    assert (
-        calc_equation(
-            equations=[["a", "b"], ["b", "c"]],
-            values=[2.0, 3.0],
-            queries=[["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]],
-        )
-        == [6, 0.5, -1, 1, -1]
-    )
-    assert (
-        calc_equation(
-            equations=[["a", "b"], ["b", "c"], ["bc", "cd"]],
-            values=[1.5, 2.5, 5.0],
-            queries=[["a", "c"], ["c", "b"], ["bc", "cd"], ["cd", "bc"]],
-        )
-        == [3.75, 0.4, 5.0, 0.2]
-    )
-    assert (
-        calc_equation(
-            equations=[["a", "b"]],
-            values=[0.5],
-            queries=[["a", "b"], ["b", "a"], ["a", "c"], ["x", "y"]],
-        )
-        == [0.5, 2, -1, -1]
-    )
+@pytest.mark.parametrize(
+    "equations, values, queries, expected",
+    [
+        (
+            [["a", "b"], ["b", "c"]],
+            [2.0, 3.0],
+            [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]],
+            [6.0, 0.5, -1, 1, -1],
+        ),
+        (
+            [["a", "b"], ["b", "c"], ["bc", "cd"]],
+            [1.5, 2.5, 5.0],
+            [["a", "c"], ["c", "b"], ["bc", "cd"], ["cd", "bc"]],
+            [3.75, 0.4, 5.0, 0.2],
+        ),
+        (
+            [["a", "b"]],
+            [0.5],
+            [["a", "b"], ["b", "a"], ["a", "c"], ["x", "y"]],
+            [0.5, 2, -1, -1],
+        ),
+    ],
+)
+def test_calc_equation(equations, values, queries, expected):
+    assert calc_equation(equations, values, queries) == expected

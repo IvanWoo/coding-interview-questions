b8782892130846a8404ede284d90d05765a7f7e8
Yifan Wu <tyivanwu@gmail.com>
2022-05-01 20:12:40 -0400

    feat: add sort_array_by_parity

---
+++ tests/test_sort_array_by_parity.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.sort_array_by_parity import sort_array_by_parity
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([3, 1, 2, 4], [2, 4, 3, 1]),
+        ([0], [0]),
+        ([1, 2, 3, 4], [2, 4, 1, 3]),
+    ],
+)
+def test_sort_array_by_parity(nums: list[int], expected: list[int]) -> None:
+    assert sort_array_by_parity(nums) == expected

7f4c2239b1005b030353e63d0963379b71bedf2a
Yifan Wu <tyivanwu@gmail.com>
2022-05-03 22:16:54 -0400

    feat: add max_number_of_k_sum_pairs

---
+++ tests/test_max_number_of_k-sum_pairs.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.max_number_of_k_sum_pairs import max_operations
+
+
+@pytest.mark.parametrize(
+    "nums, k, expected",
+    [
+        ([1, 2, 3, 4], 5, 2),
+        ([3, 1, 3, 4, 3], 6, 1),
+        ([1, 1, 1, 1, 1], 2, 2),
+    ],
+)
+def test_max_operations(nums, k, expected):
+    assert max_operations(nums, k) == expected

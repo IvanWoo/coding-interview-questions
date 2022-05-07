bd4e32cc2b83642ecce08e751adc5fb2075b80ea
Yifan Wu <tyivanwu@gmail.com>
2022-05-06 21:46:42 -0400

    refactor: update test_find132pattern

--- tests/test_find132pattern.py
+++ tests/test_find132pattern.py
@@ -1,9 +1,17 @@
+import pytest
 from puzzles.find132pattern import find132pattern
 
 
-def test_find132pattern():
-    assert find132pattern([3, 5, 0, 3, 4]) == True
-    assert find132pattern([3, 5, 0, 2, 1]) == True
-    assert find132pattern([1, 2, 3, 4]) == False
-    assert find132pattern([-1, 3, 2, 0]) == True
-    assert find132pattern([1, 0, 1, -4, -3]) == False
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([3, 5, 0, 3, 4], True),
+        ([3, 5, 0, 2, 1], True),
+        ([1, 2, 3, 4], False),
+        ([-1, 3, 2, 0], True),
+        ([1, 0, 1, -4, -3], False),
+        ([1, 1, 1, 1, 1], False),
+    ],
+)
+def test_find132pattern(nums, expected):
+    assert find132pattern(nums) == expected

2d20b4e347d616641efffc9bc2ecce4482eba2f7
Yifan Wu <tyivanwu@gmail.com>
2022-05-09 22:29:47 -0400

    feat: add combination_sum_iii

---
+++ tests/test_combination_sum_iii.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.combination_sum_iii import combination_sum_3
+
+
+@pytest.mark.parametrize(
+    "k, n, expected",
+    [
+        (3, 7, [[1, 2, 4]]),
+        (3, 9, [[1, 2, 6], [1, 3, 5], [2, 3, 4]]),
+        (4, 1, []),
+    ],
+)
+def test_combination_sum_3(k, n, expected):
+    assert combination_sum_3(k, n) == expected

6b09e0f2d2b551dd56019515f7191fd7e98ddcf1
Yifan Wu <tyivanwu@gmail.com>
2022-05-16 21:15:50 -0400

    feat: add find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree

---
+++ puzzles/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
@@ -0,0 +1,80 @@
+# https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+"""
+Given two binary trees original and cloned and given a reference to a node target in the original tree.
+
+The cloned tree is a copy of the original tree.
+
+Return a reference to the same node in the cloned tree.
+
+Note that you are not allowed to change any of the two trees or the target node and the answer must be a reference to a node in the cloned tree.
+
+
+Example 1:
+Input: tree = [7,4,3,null,null,6,19], target = 3
+Output: 3
+Explanation: In all examples the original and cloned trees are shown. The target node is a green node from the original tree. The answer is the yellow node from the cloned tree.
+
+Example 2:
+Input: tree = [7], target =  7
+Output: 7
+
+Example 3:
+Input: tree = [8,null,6,null,5,null,4,null,3,null,2,null,1], target = 4
+Output: 4
+ 
+
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+The values of the nodes of the tree are unique.
+target node is a node from the original tree and is not null.
+
+Follow up: Could you solve the problem if repeated values on the tree are allowed?
+"""
+from puzzles.utils import TreeNode
+
+
+def get_target_copy(original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+    def traverse(node):
+        nonlocal res
+        if res:
+            return
+        if not node:
+            return
+        if node.val == target.val:
+            res = node
+            return
+        traverse(node.left)
+        traverse(node.right)
+
+    res = None
+    traverse(cloned)
+    return res
+
+
+def get_target_copy(original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+    def is_same(n1, n2):
+        if not n1 and not n2:
+            return True
+        if not n1 or not n2:
+            return False
+        return (
+            n1.val == n2.val
+            and is_same(n1.left, n2.left)
+            and is_same(n1.right, n2.right)
+        )
+
+    def traverse(node):
+        nonlocal res
+        if res:
+            return
+        if not node:
+            return
+        if is_same(node, target):
+            res = node
+            return
+        traverse(node.left)
+        traverse(node.right)
+
+    res = None
+    traverse(cloned)
+    return res
---
+++ tests/test_find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
@@ -0,0 +1,42 @@
+from copy import deepcopy
+import pytest
+from puzzles.utils import TreeNode
+from puzzles.find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree import (
+    get_target_copy,
+)
+
+
+@pytest.fixture
+def tree():
+    return TreeNode(
+        7,
+        TreeNode(
+            4,
+        ),
+        TreeNode(3, TreeNode(6, TreeNode(19))),
+    )
+
+
+@pytest.fixture
+def tree_non_unique():
+    return TreeNode(
+        7,
+        TreeNode(
+            4,
+        ),
+        TreeNode(7, TreeNode(6, TreeNode(19))),
+    )
+
+
+def test_get_target_copy(tree):
+    original = deepcopy(tree)
+    cloned = deepcopy(tree)
+    target = cloned.right
+    assert get_target_copy(original, cloned, target) is target
+
+
+def test_get_target_copy_non_unique(tree_non_unique):
+    original = deepcopy(tree_non_unique)
+    cloned = deepcopy(tree_non_unique)
+    target = cloned.right
+    assert get_target_copy(original, cloned, target) is target

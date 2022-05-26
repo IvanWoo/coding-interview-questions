0fdbd131bc60ff0f0d780904d618a7ca3ede35d9
Yifan Wu <tyivanwu@gmail.com>
2022-05-25 21:37:52 -0400

    feat: add number_of_1_bits

---
+++ tests/test_number_of_1_bits.py
@@ -0,0 +1,18 @@
+from pickletools import pyset
+import pytest
+from puzzles.number_of_1_bits import hamming_weight
+
+
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (0, 0),
+        (1, 1),
+        (2, 1),
+        (3, 2),
+        (4, 1),
+        (5, 2),
+    ],
+)
+def test_hamming_weight(n, expected):
+    assert hamming_weight(n) == expected

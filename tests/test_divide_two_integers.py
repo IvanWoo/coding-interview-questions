a359f55ec4f7b6985c65fd7b687a47e0acc7c38b
Yifan Wu <tyivanwu@gmail.com>
2022-05-30 09:34:46 -0400

    feat: add divide_two_integers

---
+++ tests/test_divide_two_integers.py
@@ -0,0 +1,18 @@
+import pytest
+from puzzles.divide_two_integers import divide
+
+
+@pytest.mark.parametrize(
+    "dividend, divisor, expected",
+    [
+        (10, 3, 3),
+        (7, -3, -2),
+        (1, 1, 1),
+        (1, -1, -1),
+        (1, -2, -1),
+        (2147483647, 1, 2147483647),
+        (-2147483648, -1, 2147483647),
+    ],
+)
+def test_divide(dividend, divisor, expected):
+    assert divide(dividend, divisor) == expected

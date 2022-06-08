834ba1ca0e77a96d875c33dcd8d8b2dce7d10e10
Yifan Wu <tyivanwu@gmail.com>
2022-06-07 21:51:55 -0400

    feat: add remove_palindromic_subsequences

---
+++ tests/test_remove_palindromic_subsequences.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.remove_palindromic_subsequences import remove_palindrome_sub
+
+
+@pytest.mark.parametrize(
+    "s, expected",
+    [
+        ("ababa", 1),
+        ("abb", 2),
+        ("baabb", 2),
+    ],
+)
+def test_remove_palindrome_sub(s: str, expected: int) -> None:
+    assert remove_palindrome_sub(s) == expected

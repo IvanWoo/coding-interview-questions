b50ca741b25d85f24656916a3e0061038e5a116e
Yifan Wu <tyivanwu@gmail.com>
2022-06-23 21:46:18 -0400

    feat: add construct_target_array_with_multiple_sums

---
+++ tests/test_construct_target_array_with_multiple_sums.py
@@ -0,0 +1,18 @@
+import pytest
+from puzzles.construct_target_array_with_multiple_sums import is_possible
+
+
+@pytest.mark.parametrize(
+    "target, expected",
+    [
+        ([9, 3, 5], True),
+        ([17, 3, 5], True),
+        ([1, 1, 1, 2], False),
+        ([2], False),
+        ([8, 5], True),
+        ([1, 1000000000], True),
+        ([2, 900000002], False),
+    ],
+)
+def test_is_possible(target, expected):
+    assert is_possible(target) == expected

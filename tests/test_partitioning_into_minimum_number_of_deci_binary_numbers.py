d03a29879b1b23a4055aea2f1e5eb0c90cc99e40
Yifan Wu <tyivanwu@gmail.com>
2022-06-26 20:09:23 -0400

    feat: add partitioning_into_minimum_number_of_deci_binary_numbers

---
+++ tests/test_partitioning_into_minimum_number_of_deci_binary_numbers.py
@@ -0,0 +1,16 @@
+import pytest
+from puzzles.partitioning_into_minimum_number_of_deci_binary_numbers import (
+    min_partitions,
+)
+
+
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        ("32", 3),
+        ("82734", 8),
+        ("27346209830709182346", 9),
+    ],
+)
+def test_min_partitions(n, expected):
+    assert min_partitions(n) == expected

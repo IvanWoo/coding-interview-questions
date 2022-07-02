32b95137f3412b0e123fd2dd24e65ca62e059258
Yifan Wu <tyivanwu@gmail.com>
2022-07-01 20:35:01 -0400

    feat: add maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts

---
+++ tests/test_maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts.py
@@ -0,0 +1,17 @@
+import pytest
+from puzzles.maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts import (
+    max_area,
+)
+
+
+@pytest.mark.parametrize(
+    "h, w, horizontal_cuts, vertical_cuts, expected",
+    [
+        (5, 4, [1, 2, 4], [1, 3], 4),
+        (5, 4, [3, 1], [1], 6),
+        (5, 4, [3], [3], 9),
+        (1000000000, 1000000000, [2], [2], 81),
+    ],
+)
+def test_max_area(h, w, horizontal_cuts, vertical_cuts, expected):
+    assert max_area(h, w, horizontal_cuts, vertical_cuts) == expected

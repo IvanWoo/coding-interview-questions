ccedb5e9914f64063e5b5c334c9971d3a7cdd3f2
Yifan Wu <tyivanwu@gmail.com>
2022-07-05 20:25:22 -0400

    feat: add fibonacci_number

---
+++ tests/test_fibonacci_number.py
@@ -0,0 +1,15 @@
+import pytest
+from puzzles.fibonacci_number import fib
+
+
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (0, 0),
+        (1, 1),
+        (2, 1),
+        (30, 832040),
+    ],
+)
+def test_fib(n, expected):
+    assert fib(n) == expected

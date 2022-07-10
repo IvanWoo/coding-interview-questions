e9e07ba311070404c7249da410bff398a07a6f14
Yifan Wu <tyivanwu@gmail.com>
2022-07-09 21:27:54 -0400

    feat: add min_cost_climbing_stairs

---
+++ tests/test_min_cost_climbing_stairs.py
@@ -0,0 +1,13 @@
+import pytest
+from puzzles.min_cost_climbing_stairs import min_cost_climbing_stairs
+
+
+@pytest.mark.parametrize(
+    "cost, expected",
+    [
+        ([10, 15, 20], 15),
+        ([1, 100, 1, 1, 1, 100, 1, 1, 100, 1], 6),
+    ],
+)
+def test_min_cost_climbing_stairs(cost, expected):
+    assert min_cost_climbing_stairs(cost) == expected

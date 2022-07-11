d70f4b78417d328b99589c523f7023c464be6627
Yifan Wu <tyivanwu@gmail.com>
2022-07-10 22:21:34 -0400

    chore: update commit

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -2,12 +2,17 @@
 set -euo pipefail
 
 main() {
-    PUZZLE_NAME="$(git status | grep "new file" | awk -F "puzzles/" '{print $2}' | awk -F "." '{print $1}' | sed -n '/./{p;q;}')"
-    if [ -z "${PUZZLE_NAME}" ]; then
-        echo "No puzzle name found."
-        exit 1
+    NEW_PUZZLE="$(git status | grep "new file" | wc -l)"
+    if [ "$NEW_PUZZLE" -ge 1 ]; then
+        PUZZLE_NAME="$(git status | grep "new file" | awk -F "puzzles/" '{print $2}' | awk -F "." '{print $1}' | sed -n '/./{p;q;}')"
+        git commit -m "feat: add ${PUZZLE_NAME}"
     fi
-    git commit -m "feat: add ${PUZZLE_NAME}"
+    EDIT_PUZZLE="$(git status | grep "modified" | wc -l)"
+    if [ "$EDIT_PUZZLE" -ge 1 ]; then
+        PUZZLE_NAME="$(git status | grep "modified" | awk -F "puzzles/" '{print $2}' | awk -F "." '{print $1}' | sed -n '/./{p;q;}')"
+        git commit -m "refactor: revisit ${PUZZLE_NAME}"
+    fi
+
     git whatchanged -1 --format=oneline
 }
 

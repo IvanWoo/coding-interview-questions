a7f1ac732788b78a53e1d305a767442b80a1cbef
Yifan Wu <tyivanwu@gmail.com>
2022-07-10 22:39:07 -0400

    chore: update commit

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,17 +1,38 @@
 #!/bin/sh
 set -euo pipefail
 
+grep_file() {
+    local grep_str="$1"
+    local commit_prefix="$2"
+    TARGET="$(git status | grep $grep_str | wc -l)"
+    if [ "$TARGET" -ge 1 ]; then
+        PUZZLE_NAME="$(git status | grep $grep_str | awk -F "puzzles/" '{print $2}' | awk -F "." '{print $1}' | sed -n '/./{p;q;}')"
+        git commit -m "$commit_prefix ${PUZZLE_NAME}"
+    fi
+}
+
+commit_new_file() {
+    grep_file "new file" "feat: add"
+}
+
+commit_modified_file() {
+    grep_file "modified" "refactor: revisit"
+}
+
 main() {
-    NEW_PUZZLE="$(git status | grep "new file" | wc -l)"
-    if [ "$NEW_PUZZLE" -ge 1 ]; then
-        PUZZLE_NAME="$(git status | grep "new file" | awk -F "puzzles/" '{print $2}' | awk -F "." '{print $1}' | sed -n '/./{p;q;}')"
-        git commit -m "feat: add ${PUZZLE_NAME}"
-    fi
-    EDIT_PUZZLE="$(git status | grep "modified" | wc -l)"
-    if [ "$EDIT_PUZZLE" -ge 1 ]; then
-        PUZZLE_NAME="$(git status | grep "modified" | awk -F "puzzles/" '{print $2}' | awk -F "." '{print $1}' | sed -n '/./{p;q;}')"
-        git commit -m "refactor: revisit ${PUZZLE_NAME}"
-    fi
+    local cmd="$1"
+    case "$cmd" in
+    new)
+        commit_new_file
+        ;;
+    modified)
+        commit_modified_file
+        ;;
+    *)
+        echo "Usage: $0 {new|modified}"
+        exit 1
+        ;;
+    esac
 
     git whatchanged -1 --format=oneline
 }

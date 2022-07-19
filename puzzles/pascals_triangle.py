c7e96ba0dcce484f304ee5bef88d938c546fa6b4
Yifan Wu <tyivanwu@gmail.com>
2022-07-18 20:33:08 -0400

    feat: add pascals_triangle

---
+++ puzzles/pascals_triangle.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/pascals-triangle/
+"""
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+Input: numRows = 1
+Output: [[1]]
+ 
+Constraints:
+1 <= numRows <= 30
+"""
+
+
+def generate(num_rows: int) -> list[list[int]]:
+    ans = []
+    for n in range(num_rows + 1):
+        ans.append([1] * n)
+
+    for n in range(3, num_rows + 1):
+        for c in range(1, n - 1):
+            ans[n][c] = ans[n - 1][c - 1] + ans[n - 1][c]
+
+    return ans[1:]
+
+
+if __name__ == "__main__":
+    generate(5)
---
+++ tests/test_pascals_triangle.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.pascals_triangle import generate
+
+
+@pytest.mark.parametrize(
+    "num_rows, expected",
+    [
+        (5, [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]),
+        (1, [[1]]),
+        (2, [[1], [1, 1]]),
+    ],
+)
+def test_generate(num_rows, expected):
+    assert generate(num_rows) == expected

c75dcdb9d3b0a678adb2b0524431c094dd105d2b
Yifan Wu <tyivanwu@gmail.com>
2022-08-05 20:54:01 -0400

    feat: add poor_pigs

---
+++ tests/test_poor_pigs.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.poor_pigs import poor_pigs
+
+
+@pytest.mark.parametrize(
+    "buckets, minutes_to_die, minutes_to_test, expected",
+    [
+        (1000, 15, 60, 5),
+        (4, 15, 15, 2),
+        (4, 15, 30, 2),
+    ],
+)
+def test_poor_pigs(buckets, minutes_to_die, minutes_to_test, expected):
+    assert poor_pigs(buckets, minutes_to_die, minutes_to_test) == expected

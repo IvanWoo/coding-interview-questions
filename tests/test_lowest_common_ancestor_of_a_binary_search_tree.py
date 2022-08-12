babce6287af30c47d958df99581ca1ddaad80d64
Yifan Wu <tyivanwu@gmail.com>
2022-08-11 21:53:43 -0400

    feat: add lowest_common_ancestor_of_a_binary_search_tree

---
+++ tests/test_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,20 @@
+import pytest
+from puzzles.utils import make_tree
+from puzzles.lowest_common_ancestor_of_a_binary_search_tree import (
+    lowest_common_ancestor,
+)
+
+
+@pytest.mark.parametrize(
+    "root, p, q, expected",
+    [
+        ([6, 2, 8, 0, 4, 7, 9, None, None, 3, 5], 2, 8, 6),
+        ([6, 2, 8, 0, 4, 7, 9, None, None, 3, 5], 2, 4, 2),
+        ([2, 1], 2, 1, 2),
+    ],
+)
+def test_lowest_common_ancestor(root, p, q, expected):
+    assert (
+        lowest_common_ancestor(make_tree(root), make_tree([p]), make_tree([q])).val
+        == expected
+    )

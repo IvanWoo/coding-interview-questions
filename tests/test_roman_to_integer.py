2b367d330ee3e3c59935993151c338f125b313a9
Yifan Wu <tyivanwu@gmail.com>
2022-08-14 21:26:25 -0400

    feat: add roman_to_integer

---
+++ tests/test_roman_to_integer.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.roman_to_integer import roman_to_int
+
+
+@pytest.mark.parametrize(
+    "s, expected",
+    [
+        ("III", 3),
+        ("LVIII", 58),
+        ("MCMXCIV", 1994),
+    ],
+)
+def test_roman_to_int(s, expected):
+    assert roman_to_int(s) == expected

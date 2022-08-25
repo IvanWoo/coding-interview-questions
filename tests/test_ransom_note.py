89f511ddc085aa866ab6611b20bf10fcc60765b6
Yifan Wu <tyivanwu@gmail.com>
2022-08-24 20:19:02 -0400

    feat: add ransom_note

---
+++ tests/test_ransom_note.py
@@ -0,0 +1,15 @@
+from cmath import exp
+import pytest
+from puzzles.ransom_note import can_construct
+
+
+@pytest.mark.parametrize(
+    "random_note, magazine, expected",
+    [
+        ("a", "b", False),
+        ("aa", "ab", False),
+        ("aa", "aab", True),
+    ],
+)
+def test_can_construct(random_note, magazine, expected):
+    assert can_construct(random_note, magazine) == expected

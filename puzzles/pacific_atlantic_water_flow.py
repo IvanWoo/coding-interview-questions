8ce716fe4e96c0bf019f52bec8daf35d3f3d5d6e
Yifan Wu <tyivanwu@gmail.com>
2022-08-30 21:16:21 -0400

    refactor: revisit pacific_atlantic_water_flow

--- puzzles/pacific_atlantic_water_flow.py
+++ puzzles/pacific_atlantic_water_flow.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/pacific-atlantic-water-flow/
 """
-Given an m x n matrix of non-negative integers representing the height of each unit cell in a continent, the "Pacific ocean" touches the left and top edges of the matrix and the "Atlantic ocean" touches the right and bottom edges.
+Given an m x n heights of non-negative integers representing the height of each unit cell in a continent, the "Pacific ocean" touches the left and top edges of the heights and the "Atlantic ocean" touches the right and bottom edges.
 
 Water can only flow in four directions (up, down, left, or right) from a cell to another one with height equal or lower.
 
@@ -14,7 +14,7 @@ Both m and n are less than 150.
 
 Example:
 
-Given the following 5x5 matrix:
+Given the following 5x5 heights:
 
   Pacific ~   ~   ~   ~   ~ 
        ~  1   2   2   3  (5) *
@@ -26,16 +26,15 @@ Given the following 5x5 matrix:
 
 Return:
 
-[[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).
+[[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above heights).
 """
-from typing import List
 from collections import defaultdict
 
 
-def pacific_atlantic(matrix: List[List[int]]) -> List[List[int]]:
+def pacific_atlantic(heights: list[list[int]]) -> list[list[int]]:
     def helper(r, c):
         nonlocal memo, graph
-        row, col = len(matrix), len(matrix[0])
+        row, col = len(heights), len(heights[0])
         visited = set()
         q = [(r, c)]
         is_p, is_a = False, False
@@ -55,11 +54,11 @@ def pacific_atlantic(matrix: List[List[int]]) -> List[List[int]]:
             q.extend(graph[(cur_row, cur_col)])
         return False
 
-    if not matrix:
+    if not heights:
         return []
     res = []
     memo = dict()
-    row, col = len(matrix), len(matrix[0])
+    row, col = len(heights), len(heights[0])
 
     # build the graph
     graph = defaultdict(list)
@@ -68,7 +67,7 @@ def pacific_atlantic(matrix: List[List[int]]) -> List[List[int]]:
             for i, j in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 new_r, new_c = r + i, c + j
                 if 0 <= new_r < row and 0 <= new_c < col:
-                    if matrix[new_r][new_c] <= matrix[r][c]:
+                    if heights[new_r][new_c] <= heights[r][c]:
                         graph[(r, c)].append((new_r, new_c))
 
     for r in range(row):
@@ -78,6 +77,40 @@ def pacific_atlantic(matrix: List[List[int]]) -> List[List[int]]:
             if is_connected:
                 res.append([r, c])
     return res
+
+
+def pacific_atlantic(heights: list[list[int]]) -> list[list[int]]:
+    def verify(r, c):
+        return (r == 0 or c == 0, r == rows - 1 or c == cols - 1)
+
+    def helper(r, c, visited):
+        nonlocal ans
+        touch_p, touch_a = verify(r, c)
+        if (r, c) in ans:
+            return [True, True]
+        visited.add((r, c))
+        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols:
+                if (nr, nc) in visited:
+                    continue
+                if heights[nr][nc] > heights[r][c]:
+                    continue
+                p, a = helper(nr, nc, visited)
+                touch_p = any([touch_p, p])
+                touch_a = any([touch_a, a])
+                if all([touch_p, touch_a]):
+                    return touch_p, touch_a
+        return touch_p, touch_a
+
+    rows, cols = len(heights), len(heights[0])
+    ans = []
+    for r in range(rows):
+        for c in range(cols):
+            if all(helper(r, c, set())):
+                ans.append((r, c))
+
+    return [list(x) for x in ans]
 
 
 if __name__ == "__main__":
--- tests/test_pacific_atlantic_water_flow.py
+++ tests/test_pacific_atlantic_water_flow.py
@@ -1,18 +1,22 @@
+import pytest
 from puzzles.pacific_atlantic_water_flow import pacific_atlantic
 
 
-def test_pacific_atlantic():
-    assert (
-        sorted(
-            pacific_atlantic(
-                [
-                    [1, 2, 2, 3, 5],
-                    [3, 2, 3, 4, 4],
-                    [2, 4, 5, 3, 1],
-                    [6, 7, 1, 4, 5],
-                    [5, 1, 1, 2, 4],
-                ]
-            )
-        )
-        == sorted([[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]])
-    )
+@pytest.mark.parametrize(
+    "heights, expected",
+    [
+        (
+            [
+                [1, 2, 2, 3, 5],
+                [3, 2, 3, 4, 4],
+                [2, 4, 5, 3, 1],
+                [6, 7, 1, 4, 5],
+                [5, 1, 1, 2, 4],
+            ],
+            [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]],
+        ),
+        ([[1]], [[0, 0]]),
+    ],
+)
+def test_pacific_atlantic(heights, expected):
+    assert sorted(pacific_atlantic(heights)) == sorted(expected)

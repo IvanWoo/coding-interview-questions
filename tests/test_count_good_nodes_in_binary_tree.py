942de230ad826112680a912961e53a2f5f83df66
Yifan Wu <tyivanwu@gmail.com>
2022-08-31 20:53:01 -0400

    feat: add count_good_nodes_in_binary_tree

---
+++ tests/test_count_good_nodes_in_binary_tree.py
@@ -0,0 +1,16 @@
+from cmath import exp
+import pytest
+from puzzles.utils import make_tree
+from puzzles.count_good_nodes_in_binary_tree import good_nodes
+
+
+@pytest.mark.parametrize(
+    "root, expected",
+    [
+        (make_tree([3, 1, 4, 3, None, 1, 5]), 4),
+        (make_tree([3, 3, None, 4, 2]), 3),
+        (make_tree([1]), 1),
+    ],
+)
+def test_good_nodes(root, expected):
+    assert good_nodes(root) == expected

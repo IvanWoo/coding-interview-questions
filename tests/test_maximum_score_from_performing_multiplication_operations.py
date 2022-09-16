f7ebfae0b6c81b1889e25a1293177444a5fe19b2
Yifan Wu <tyivanwu@gmail.com>
2022-09-15 21:55:07 -0400

    feat: add maximum_score_from_performing_multiplication_operations

---
+++ tests/test_maximum_score_from_performing_multiplication_operations.py
@@ -0,0 +1,15 @@
+import pytest
+from puzzles.maximum_score_from_performing_multiplication_operations import (
+    maximum_score,
+)
+
+
+@pytest.mark.parametrize(
+    "nums, multipliers, expected",
+    [
+        ([1, 2, 3], [3, 2, 1], 14),
+        ([-5, -3, -3, -2, 7, 1], [-10, -5, 3, 4, 6], 102),
+    ],
+)
+def test_maximum_score(nums, multipliers, expected):
+    assert maximum_score(nums, multipliers) == expected

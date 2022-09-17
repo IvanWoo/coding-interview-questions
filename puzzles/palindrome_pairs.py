4e003c9a085ef431105e1daf2f8822c41e2246c1
Yifan Wu <tyivanwu@gmail.com>
2022-09-16 22:17:03 -0400

    refactor: revisit palindrome_pairs

--- puzzles/palindrome_pairs.py
+++ puzzles/palindrome_pairs.py
@@ -21,10 +21,13 @@ Constraints:
 0 <= words[i].length <= 300
 words[i] consists of lower-case English letters.
 """
-from typing import List
+
+from __future__ import annotations
+from dataclasses import dataclass, field
+
 
 # TLE
-def palindrome_pairs(words: List[str]) -> List[List[int]]:
+def palindrome_pairs(words: list[str]) -> list[list[int]]:
     def is_palindrome(s):
         return s == s[::-1]
 
@@ -39,70 +42,96 @@ def palindrome_pairs(words: List[str]) -> List[List[int]]:
     return res
 
 
-def palindrome_pairs(words: List[str]) -> List[List[int]]:
-    def partial_palindrome(word, left):
-        # test word[left:] is parlindrom or not
-        right = len(word) - 1
-        while left <= right:
-            if word[left] != word[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
-
+def palindrome_pairs(words: list[str]) -> list[list[int]]:
+    @dataclass
     class TrieNode:
-        def __init__(self):
-            self.word_index = -1
-            self.value = None
-            self.list = []
-            self.children = dict()
+        word_idx: int = -1
+        children: dict[str, TrieNode] = field(default_factory=dict)
+        rest_is_palindrome: list[int] = field(default_factory=list)
 
     class Trie:
         def __init__(self):
             self.root = TrieNode()
 
-        def insert(self, word, index):
-            node = self.root
-            for j, char in enumerate(word):
-                if partial_palindrome(word, j):
-                    node.list.append(index)
-                if char not in node.children:
-                    node.children[char] = TrieNode()
-                node = node.children[char]
-            node.word_index = index
-            node.value = word
+        def is_palindrome(self, word: str, lo: int, hi: int) -> bool:
+            while lo <= hi:
+                if word[lo] != word[hi]:
+                    return False
+                lo += 1
+                hi -= 1
+            return True
 
-    def get_candidates(trie, word, i):
-        candidates = []
-        node = trie.root
-        reversed_word = word[::-1]
-        for j, char in enumerate(reversed_word):
-            # reversed_word is longer than canadidate
-            if node.word_index != -1 and partial_palindrome(reversed_word, j):
-                candidates.append(node.word_index)
-            if char not in node.children:
-                break
-            node = node.children[char]
-        else:
-            # canadidate == reversed_word[::-1]
-            if node.word_index not in [-1, i]:
-                candidates.append(node.word_index)
-            # canadidate = abc|sos, reversed_word = cba
-            for k in node.list:
-                candidates.append(k)
-        return candidates
+        def add(self, word: str, idx: int):
+            cur = self.root
+            # add the word reversely to make the search feasible
+            n = len(word)
+            for i in reversed(range(n)):
+                if self.is_palindrome(word, 0, i):
+                    cur.rest_is_palindrome.append(idx)
 
-    trie = Trie()
-    for index, word in enumerate(words):
-        trie.insert(word, index)
+                char = word[i]
+                if char not in cur.children:
+                    cur.children[char] = TrieNode()
 
-    res = []
+                cur = cur.children[char]
+            cur.word_idx = idx
+
+        def search(self, word: str, idx: int, ans: list):
+            cur = self.root
+            for i, char in enumerate(word):
+                # case 0: xyzll|zyx
+                if cur.word_idx != -1 and self.is_palindrome(word, i, len(word) - 1):
+                    ans.append([idx, cur.word_idx])
+
+                if char not in cur.children:
+                    break
+
+                cur = cur.children[char]
+            else:
+                # only run when no break happened above
+                # case 1: abcd|dcba, aaa
+                if cur.word_idx != -1 and cur.word_idx != idx:
+                    ans.append([idx, cur.word_idx])
+
+                # case 2: zyx|xyzll
+                for k in cur.rest_is_palindrome:
+                    ans.append([idx, k])
+
+    t = Trie()
+    ans = []
+
     for i, word in enumerate(words):
-        candidates = get_candidates(trie, word, i)
-        res.extend([[c, i] for c in candidates])
-    return res
+        t.add(word, i)
+
+    for i, word in enumerate(words):
+        t.search(word, i, ans)
+
+    return ans
+
+
+def palindrome_pairs(words: list[str]) -> list[list[int]]:
+    d = {w: i for i, w in enumerate(words)}
+    ans = []
+    for i, w in enumerate(words):
+        # case 0:
+        rev_w = w[::-1]
+        if rev_w in d and d[rev_w] != i:
+            ans.append([i, d[rev_w]])
+        if w != "" and rev_w == w and "" in d:
+            ans.append([i, d[""]])
+            ans.append([d[""], i])
+
+        for k in range(1, len(w)):
+            # case 1:
+            s1, s2 = w[:k], w[k:]
+            if s1 == s1[::-1] and s2[::-1] in d:
+                ans.append([d[s2[::-1]], i])
+            # case 2:
+            if s2 == s2[::-1] and s1[::-1] in d:
+                ans.append([i, d[s1[::-1]]])
+
+    return ans
 
 
 if __name__ == "__main__":
-    palindrome_pairs(["a", ""])
     palindrome_pairs(["abcd", "dcba", "lls", "s", "sssll"])
--- tests/test_palindrome_pairs.py
+++ tests/test_palindrome_pairs.py
@@ -1,14 +1,14 @@
+import pytest
 from puzzles.palindrome_pairs import palindrome_pairs
 
 
-def test_palindrome_pairs():
-    assert sorted(palindrome_pairs(["abcd", "dcba", "lls", "s", "sssll"])) == sorted(
-        [
-            [0, 1],
-            [1, 0],
-            [3, 2],
-            [2, 4],
-        ]
-    )
-    assert sorted(palindrome_pairs(["bat", "tab", "cat"])) == sorted([[0, 1], [1, 0]])
-    assert sorted(palindrome_pairs(["a", ""])) == sorted([[0, 1], [1, 0]])
+@pytest.mark.parametrize(
+    "words, expected",
+    [
+        (["abcd", "dcba", "lls", "s", "sssll"], [[0, 1], [1, 0], [3, 2], [2, 4]]),
+        (["bat", "tab", "cat"], [[0, 1], [1, 0]]),
+        (["a", ""], [[0, 1], [1, 0]]),
+    ],
+)
+def test_palindrome_pairs(words, expected):
+    assert sorted(palindrome_pairs(words)) == sorted(expected)

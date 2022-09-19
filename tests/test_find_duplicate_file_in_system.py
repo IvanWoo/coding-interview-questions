28f67b83aa1c7b618055568915393fb8f5539f5e
Yifan Wu <tyivanwu@gmail.com>
2022-09-18 21:46:50 -0400

    feat: add find_duplicate_file_in_system

---
+++ tests/test_find_duplicate_file_in_system.py
@@ -0,0 +1,31 @@
+import pytest
+from puzzles.find_duplicate_file_in_system import find_duplicate
+
+
+@pytest.mark.parametrize(
+    "paths, expected",
+    [
+        (
+            [
+                "root/a 1.txt(abcd) 2.txt(efgh)",
+                "root/c 3.txt(abcd)",
+                "root/c/d 4.txt(efgh)",
+                "root 4.txt(efgh)",
+            ],
+            [
+                ["root/a/2.txt", "root/c/d/4.txt", "root/4.txt"],
+                ["root/a/1.txt", "root/c/3.txt"],
+            ],
+        ),
+        (
+            [
+                "root/a 1.txt(abcd) 2.txt(efgh)",
+                "root/c 3.txt(abcd)",
+                "root/c/d 4.txt(efgh)",
+            ],
+            [["root/a/2.txt", "root/c/d/4.txt"], ["root/a/1.txt", "root/c/3.txt"]],
+        ),
+    ],
+)
+def test_find_duplicate(paths, expected):
+    assert sorted(find_duplicate(paths)) == sorted(expected)

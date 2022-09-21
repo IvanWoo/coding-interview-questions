99dcfe77fd9833367d4564155517be7cc35d83a8
Yifan Wu <tyivanwu@gmail.com>
2022-09-20 20:37:57 -0400

    refactor: revisit add_strings

--- puzzles/add_strings.py
+++ puzzles/add_strings.py
@@ -9,6 +9,7 @@ Both num1 and num2 contains only digits 0-9.
 Both num1 and num2 does not contain any leading zero.
 You must not use any built-in BigInteger library or convert the inputs to integer directly.
 """
+from collections import defaultdict, deque
 
 
 def add_strings(num1: str, num2: str) -> str:
@@ -29,6 +30,28 @@ def add_strings(num1: str, num2: str) -> str:
         ans.append(carry)
 
     return "".join(str(x) for x in ans[::-1])
+
+
+def add_strings(num1: str, num2: str) -> str:
+    def get_val(n: str) -> dict[int, int]:
+        ans = defaultdict(int)
+        for k, v in enumerate(n):
+            ans[k] = int(v)
+        return ans
+
+    i1, i2 = len(num1) - 1, len(num2) - 1
+    v1, v2 = get_val(num1), get_val(num2)
+    carry = 0
+    ans = deque()
+    while i1 >= 0 or i2 >= 0:
+        total = carry + v1[i1] + v2[i2]
+        carry, val = divmod(total, 10)
+        ans.appendleft(val)
+        i1 -= 1
+        i2 -= 1
+    if carry:
+        ans.appendleft(carry)
+    return "".join((str(x) for x in ans))
 
 
 if __name__ == "__main__":
--- tests/test_add_strings.py
+++ tests/test_add_strings.py
@@ -2,7 +2,9 @@ import pytest
 from puzzles.add_strings import add_strings
 
 
-def test_add_strings():
-    assert add_strings("0", "0") == "0"
-    assert add_strings("9", "0") == "9"
-    assert add_strings("3099", "9") == "3108"
+@pytest.mark.parametrize(
+    "num1, num2, expected",
+    [("0", "0", "0"), ("9", "0", "9"), ("9", "1", "10"), ("3099", "9", "3108")],
+)
+def test_add_strings(num1, num2, expected):
+    assert add_strings(num1, num2) == expected

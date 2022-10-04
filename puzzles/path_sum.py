bd534342bdfb48a1418e0d596c27487a20d4be4a
Yifan Wu <tyivanwu@gmail.com>
2022-10-03 20:19:50 -0400

    feat: add path_sum

---
+++ puzzles/path_sum.py
@@ -0,0 +1,63 @@
+# https://leetcode.com/problems/path-sum/
+"""
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+
+Example 1:
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+Example 2:
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+
+Example 3:
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+ 
+
+Constraints:
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+"""
+from typing import Optional
+from puzzles.utils import TreeNode
+
+
+def has_path_sum(root: Optional[TreeNode], target_sum: int) -> bool:
+    def traverse(node, cur_sum):
+        if node and node.left is None and node.right is None:
+            return cur_sum + node.val == target_sum
+        if not node:
+            return False
+        val = node.val
+        left = traverse(node.left, cur_sum + val)
+        right = traverse(node.right, cur_sum + val)
+        return left or right
+
+    return traverse(root, 0)
+
+
+def has_path_sum(root: Optional[TreeNode], target_sum: int) -> bool:
+    def traverse(node, cur_target):
+        if not node:
+            return False
+        if node.left is None and node.right is None:
+            return node.val == cur_target
+        val = node.val
+        left = traverse(node.left, cur_target - val)
+        right = traverse(node.right, cur_target - val)
+        return left or right
+
+    return traverse(root, target_sum)
---
+++ tests/test_path_sum.py
@@ -0,0 +1,17 @@
+from ssl import HAS_ALPN
+import pytest
+from puzzles.utils import make_tree
+from puzzles.path_sum import has_path_sum
+
+
+@pytest.mark.parametrize(
+    "root, target_sum, expected",
+    [
+        (make_tree([5, 4, 8, 11, None, 13, 4, 7, 2, None, None, None, 1]), 22, True),
+        (make_tree([1, 2, 3]), 5, False),
+        (make_tree([]), 0, False),
+        (make_tree([1, 2]), 1, False),
+    ],
+)
+def test_has_path_sum(root, target_sum, expected):
+    assert has_path_sum(root, target_sum) == expected

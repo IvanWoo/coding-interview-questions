dd916c4d5b1774bcfa2b17c8a868efe933d4d14c
Yifan Wu <tyivanwu@gmail.com>
2022-10-09 20:48:41 -0400

    feat: add break_a_palindrome

---
+++ tests/test_break_a_palindrome.py
@@ -0,0 +1,15 @@
+import pytest
+from puzzles.break_a_palindrome import break_palindrome
+
+
+@pytest.mark.parametrize(
+    "palindrome, expected",
+    [
+        ("abccba", "aaccba"),
+        ("a", ""),
+        ("aa", "ab"),
+        ("aba", "abb"),
+    ],
+)
+def test_break_palindrome(palindrome, expected):
+    assert break_palindrome(palindrome) == expected

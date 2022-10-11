6b5630bdd8a66f644cb100d60144d1b4914e5334
Yifan Wu <tyivanwu@gmail.com>
2022-10-10 23:10:54 -0400

    feat: add increasing_triplet_subsequence

---
+++ tests/test_increasing_triplet_subsequence.py
@@ -0,0 +1,16 @@
+import pytest
+from puzzles.increasing_triplet_subsequence import increasing_triplet
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([1, 2, 3, 4, 5], True),
+        ([5, 4, 3, 2, 1], False),
+        ([2, 2, 2], False),
+        ([2, 1, 5, 0, 4, 6], True),
+        ([1, 2, 5, 4, 0, 6], True),
+    ],
+)
+def test_increasing_triplet(nums, expected):
+    assert increasing_triplet(nums) == expected

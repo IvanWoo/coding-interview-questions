aed305513078e8beb18ae6be4e792390ee2bb110
Yifan Wu <tyivanwu@gmail.com>
2022-10-12 21:58:19 -0400

    feat: add delete_node_in_a_linked_list

---
+++ tests/test_delete_node_in_a_linked_list.py
@@ -0,0 +1,18 @@
+import pytest
+from puzzles.utils import make_linked_list
+from puzzles.delete_node_in_a_linked_list import delete_node
+
+
+@pytest.mark.parametrize(
+    "head, node_val, expected",
+    [
+        (make_linked_list([4, 5, 1, 9]), 5, make_linked_list([4, 1, 9])),
+        (make_linked_list([4, 5, 1, 9]), 1, make_linked_list([4, 5, 9])),
+    ],
+)
+def test_delete_node(head, node_val, expected):
+    node = head
+    while node.val != node_val:
+        node = node.next
+    delete_node(node)
+    assert head == expected

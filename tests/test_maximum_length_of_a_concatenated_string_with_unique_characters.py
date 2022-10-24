060887411848a7202726e1a75d05b9745d98b529
Yifan Wu <tyivanwu@gmail.com>
2022-10-23 20:51:55 -0400

    feat: add maximum_length_of_a_concatenated_string_with_unique_characters

---
+++ tests/test_maximum_length_of_a_concatenated_string_with_unique_characters.py
@@ -0,0 +1,17 @@
+import pytest
+from puzzles.maximum_length_of_a_concatenated_string_with_unique_characters import (
+    max_length,
+)
+
+
+@pytest.mark.parametrize(
+    "arr, expected",
+    [
+        (["aa"], 0),
+        (["un", "iq", "ue"], 4),
+        (["cha", "r", "act", "ers"], 6),
+        (["abcdefghijklmnopqrstuvwxyz"], 26),
+    ],
+)
+def test_max_length(arr, expected):
+    assert max_length(arr) == expected

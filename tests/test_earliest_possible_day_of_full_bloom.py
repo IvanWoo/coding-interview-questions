3f28b71911af3016bc7c9766dbce4724747fb239
Yifan Wu <tyivanwu@gmail.com>
2022-10-28 22:46:16 -0400

    feat: add earliest_possible_day_of_full_bloom

---
+++ tests/test_earliest_possible_day_of_full_bloom.py
@@ -0,0 +1,10 @@
+import pytest
+from puzzles.earliest_possible_day_of_full_bloom import earliest_full_bloom
+
+
+@pytest.mark.parametrize(
+    "plant_time, grow_time, expected",
+    [([1, 4, 3], [2, 3, 1], 9), ([1, 2, 3, 2], [2, 1, 2, 1], 9), ([1], [1], 2)],
+)
+def test_earliest_full_bloom(plant_time, grow_time, expected):
+    assert earliest_full_bloom(plant_time, grow_time) == expected

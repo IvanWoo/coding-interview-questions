1f9a6743b5dbed4dafddeae62fde0503d72b3964
Yifan Wu <tyivanwu@gmail.com>
2022-10-30 20:25:39 -0400

    feat: add toeplitz_matrix

---
+++ tests/test_toeplitz_matrix.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.toeplitz_matrix import is_toeplitz_matrix
+
+
+@pytest.mark.parametrize(
+    "matrix, expected",
+    [
+        ([[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]], True),
+        ([[1, 2], [2, 2]], False),
+        ([[1]], True),
+    ],
+)
+def test_is_toeplitz_matrix(matrix, expected):
+    assert is_toeplitz_matrix(matrix) == expected

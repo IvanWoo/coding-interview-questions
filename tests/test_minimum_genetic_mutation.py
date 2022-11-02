76ee58f5ca164afe8a9c151eec98a0d7d0e083a2
Yifan Wu <tyivanwu@gmail.com>
2022-11-01 21:09:07 -0400

    feat: add minimum_genetic_mutation

---
+++ tests/test_minimum_genetic_mutation.py
@@ -0,0 +1,21 @@
+import pytest
+from puzzles.minimum_genetic_mutation import min_mutation
+
+
+@pytest.mark.parametrize(
+    "start, end, bank, expected",
+    [
+        ("AACCGGTT", "AACCGGTA", ["AACCGGTA"], 1),
+        ("AACCGGTT", "AAACGGTA", ["AACCGGTA", "AACCGCTA", "AAACGGTA"], 2),
+        ("AAAAACCC", "AACCCCCC", ["AAAACCCC", "AAACCCCC", "AACCCCCC"], 3),
+        ("AACCGGTT", "AAACGGTA", ["AACCGATT", "AACCGATA", "AAACGATA", "AAACGGTA"], 4),
+        (
+            "AAAAAAAA",
+            "AAAAACGG",
+            ["AAAAAAGA", "AAAAAGGA", "AAAAACGA", "AAAAACGG", "AAAAAAGG", "AAAAAAGC"],
+            3,
+        ),
+    ],
+)
+def test_min_mutation(start, end, bank, expected):
+    assert min_mutation(start, end, bank) == expected

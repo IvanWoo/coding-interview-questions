bf2c0e39fe6a6c02782aa7605709036e713e0434
Yifan Wu <tyivanwu@gmail.com>
2022-11-06 20:45:31 -0500

    feat: add maximum_69_number

---
+++ tests/test_maximum_69_number.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.maximum_69_number import maximum_69_number
+
+
+@pytest.mark.parametrize(
+    "num, expected",
+    [
+        (9669, 9969),
+        (9996, 9999),
+        (9999, 9999),
+    ],
+)
+def test_maximum_69_number(num, expected):
+    assert maximum_69_number(num) == expected

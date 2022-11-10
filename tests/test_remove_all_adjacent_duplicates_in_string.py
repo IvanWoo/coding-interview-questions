5498af1f6d17756252e6d0f3e16ed7422bdf4114
Yifan Wu <tyivanwu@gmail.com>
2022-11-09 19:53:36 -0500

    feat: add remove_all_adjacent_duplicates_in_string

---
+++ tests/test_remove_all_adjacent_duplicates_in_string.py
@@ -0,0 +1,13 @@
+import pytest
+from puzzles.remove_all_adjacent_duplicates_in_string import remove_duplicates
+
+
+@pytest.mark.parametrize(
+    "s, expected",
+    [
+        ("abbaca", "ca"),
+        ("azxxzy", "ay"),
+    ],
+)
+def test_remove_duplicates(s, expected):
+    assert remove_duplicates(s) == expected

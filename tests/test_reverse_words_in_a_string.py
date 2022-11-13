145b7c169be734d4329622dd31a57d1f32bafbc6
Yifan Wu <tyivanwu@gmail.com>
2022-11-12 20:39:18 -0500

    feat: add reverse_words_in_a_string

---
+++ tests/test_reverse_words_in_a_string.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.reverse_words_in_a_string import reverse_words
+
+
+@pytest.mark.parametrize(
+    "s, expected",
+    [
+        ("the sky is blue", "blue is sky the"),
+        ("  hello world  ", "world hello"),
+        ("a good   example", "example good a"),
+    ],
+)
+def test_reverse_words(s, expected):
+    assert reverse_words(s) == expected

c34572273a6f44aa5fd9618a3335b1e527cc8456
Yifan Wu <tyivanwu@gmail.com>
2022-11-16 22:20:41 -0500

    feat: add rectangle_area

---
+++ puzzles/rectangle_area.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/rectangle-area/
+"""
+Given the coordinates of two rectilinear rectangles in a 2D plane, return the total area covered by the two rectangles.
+
+The first rectangle is defined by its bottom-left corner (ax1, ay1) and its top-right corner (ax2, ay2).
+
+The second rectangle is defined by its bottom-left corner (bx1, by1) and its top-right corner (bx2, by2).
+
+Example 1:
+Rectangle Area
+Input: ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0, by1 = -1, bx2 = 9, by2 = 2
+Output: 45
+
+Example 2:
+Input: ax1 = -2, ay1 = -2, ax2 = 2, ay2 = 2, bx1 = -2, by1 = -2, bx2 = 2, by2 = 2
+Output: 16
+ 
+Constraints:
+-104 <= ax1 <= ax2 <= 104
+-104 <= ay1 <= ay2 <= 104
+-104 <= bx1 <= bx2 <= 104
+-104 <= by1 <= by2 <= 104
+"""
+
+
+def compute_area(
+    ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int
+) -> int:
+    def rect_area(x1, y1, x2, y2):
+        width = max((x2 - x1), 0)
+        height = max((y2 - y1), 0)
+        return width * height
+
+    a_area = rect_area(ax1, ay1, ax2, ay2)
+    b_area = rect_area(bx1, by1, bx2, by2)
+    overlap_area = rect_area(max(ax1, bx1), max(ay1, by1), min(ax2, bx2), min(ay2, by2))
+    return a_area + b_area - overlap_area
---
+++ tests/test_rectangle_area.py
@@ -0,0 +1,16 @@
+import pytest
+from puzzles.rectangle_area import compute_area
+
+
+@pytest.mark.parametrize(
+    "ax1, ay1, ax2, ay2, bx1, by1, bx2, by2, expected",
+    [
+        (-3, 0, 3, 4, 0, -1, 9, 2, 45),
+        (0, -1, 9, 2, -3, 0, 3, 4, 45),
+        (-2, -2, 2, 2, -2, -2, 2, 2, 16),
+        (-2, -2, 2, 2, 3, 3, 4, 4, 17),
+        (-2, -2, 2, 2, -1, -1, 1, 1, 16),
+    ],
+)
+def test_compute_area(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2, expected):
+    assert compute_area(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) == expected

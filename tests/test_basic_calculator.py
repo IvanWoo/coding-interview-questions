ca8f639e4420dc60d4dae9695b8e0dff28c20b96
Yifan Wu <tyivanwu@gmail.com>
2022-11-19 21:43:46 -0500

    refactor: revisit basic_calculator

--- tests/test_basic_calculator.py
+++ tests/test_basic_calculator.py
@@ -1,14 +1,21 @@
+import pytest
 from puzzles.basic_calculator import calculate
 
 
-def test_calculate():
-    assert calculate("-2") == -2
-    assert calculate("42") == 42
-    assert calculate("1 + 1") == 2
-    assert calculate(" 1+1") == 2
-    assert calculate(" 2-1 + 2 ") == 3
-    assert calculate("(6+8)") == 14
-    assert calculate("(4+5- 2)") == 7
-    assert calculate("1+(4+5+2)-3") == 9
-    assert calculate("(1+(4+5+2)-3)") == 9
-    assert calculate("(1+(4+5+2)-3)+(6+8)") == 23
+@pytest.mark.parametrize(
+    "s, expected",
+    [
+        ("-2", -2),
+        ("42", 42),
+        ("1 + 1", 2),
+        (" 1+1", 2),
+        (" 2-1 + 2 ", 3),
+        ("(6+8)", 14),
+        ("(4+5- 2)", 7),
+        ("1+(4+5+2)-3", 9),
+        ("(1+(4+5+2)-3)", 9),
+        ("(1+(4+5+2)-3)+(6+8)", 23),
+    ],
+)
+def test_calculate(s, expected):
+    assert calculate(s) == expected

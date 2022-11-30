4c1f61565d6e610ab0f8a21687107352389ebbe9
Yifan Wu <tyivanwu@gmail.com>
2022-11-29 19:21:54 -0500

    feat: add unique_number_of_occurrences

---
+++ tests/test_unique_number_of_occurrences.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.unique_number_of_occurrences import unique_occurrences
+
+
+@pytest.mark.parametrize(
+    "arr, expected",
+    [
+        ([1, 2, 2, 1, 1, 3], True),
+        ([1, 2], False),
+        ([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0], True),
+    ],
+)
+def test_unique_occurrences(arr, expected):
+    assert unique_occurrences(arr) == expected

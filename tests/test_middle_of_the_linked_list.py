3990dcbd8c13cf3e17519d4aa195c746aab509db
Yifan Wu <tyivanwu@gmail.com>
2022-12-04 20:30:08 -0500

    feat: add middle_of_the_linked_list

---
+++ tests/test_middle_of_the_linked_list.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.utils import make_linked_list
+from puzzles.middle_of_the_linked_list import middle_node
+
+
+@pytest.mark.parametrize(
+    "head, expected",
+    [
+        (make_linked_list([1, 2, 3, 4, 5]), make_linked_list([3, 4, 5])),
+        (make_linked_list([1, 2, 3, 4, 5, 6]), make_linked_list([4, 5, 6])),
+    ],
+)
+def test_middle_node(head, expected):
+    assert middle_node(head) == expected

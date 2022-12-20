3f6143174b6947cf01d364b08d2684809abce075
Yifan Wu <tyivanwu@gmail.com>
2022-12-19 20:44:32 -0500

    feat: add keys_and_rooms

---
+++ tests/test_keys_and_rooms.py
@@ -0,0 +1,13 @@
+import pytest
+from puzzles.keys_and_rooms import can_visit_all_rooms
+
+
+@pytest.mark.parametrize(
+    "rooms, expected",
+    [
+        ([[1], [2], [3], []], True),
+        ([[1, 3], [3, 0, 1], [2], [0]], False),
+    ],
+)
+def test_can_visit_all_rooms(rooms, expected):
+    assert can_visit_all_rooms(rooms) == expected

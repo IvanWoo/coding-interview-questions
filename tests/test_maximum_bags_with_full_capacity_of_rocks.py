9ad3f32ddfce642caeacba562e6862b13c76a89b
Yifan Wu <tyivanwu@gmail.com>
2022-12-26 21:40:11 -0500

    feat: add maximum_bags_with_full_capacity_of_rocks

---
+++ tests/test_maximum_bags_with_full_capacity_of_rocks.py
@@ -0,0 +1,13 @@
+import pytest
+from puzzles.maximum_bags_with_full_capacity_of_rocks import maximum_bags
+
+
+@pytest.mark.parametrize(
+    "capacity, rocks, additional_rocks, expected",
+    [
+        ([2, 3, 4, 5], [1, 2, 4, 4], 2, 3),
+        ([10, 2, 2], [2, 2, 0], 100, 3),
+    ],
+)
+def test_maximum_bags(capacity, rocks, additional_rocks, expected):
+    assert maximum_bags(capacity, rocks, additional_rocks) == expected

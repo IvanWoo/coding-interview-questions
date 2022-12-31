25184a194a127721517bcd4ca8d5d27ee3efbe29
Yifan Wu <tyivanwu@gmail.com>
2022-12-30 21:28:56 -0500

    feat: add unique_paths_iii

---
+++ puzzles/unique_paths_iii.py
@@ -0,0 +1,72 @@
+# https://leetcode.com/problems/unique-paths-iii/description/
+"""
+You are given an m x n integer array grid where grid[i][j] could be:
+
+1 representing the starting square. There is exactly one starting square.
+2 representing the ending square. There is exactly one ending square.
+0 representing empty squares we can walk over.
+-1 representing obstacles that we cannot walk over.
+Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
+
+Example 1:
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+Output: 2
+Explanation: We have the following two paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+
+Example 2:
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+Output: 4
+Explanation: We have the following four paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+
+Example 3:
+Input: grid = [[0,1],[2,0]]
+Output: 0
+Explanation: There is no path that walks over every empty square exactly once.
+Note that the starting and ending square can be anywhere in the grid.
+ 
+
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 20
+1 <= m * n <= 20
+-1 <= grid[i][j] <= 2
+There is exactly one starting cell and one ending cell.
+"""
+
+# brute force
+def unique_paths_iii(grid: list[list[int]]) -> int:
+    def helper(path):
+        nonlocal ans
+        if path[-1] == end:
+            if len(path) == rows * cols - obstacles:
+                ans.add(tuple(path))
+            return
+        r, c = path[-1]
+        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols:
+                if grid[nr][nc] != -1 and (nr, nc) not in path:
+                    helper(path + [(nr, nc)])
+
+    rows, cols = len(grid), len(grid[0])
+    start = end = None
+    obstacles = 0
+    for r in range(rows):
+        for c in range(cols):
+            match grid[r][c]:
+                case 1:
+                    start = (r, c)
+                case 2:
+                    end = (r, c)
+                case -1:
+                    obstacles += 1
+    ans = set()
+    helper([start])
+    return len(ans)
---
+++ tests/test_unique_paths_iii.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.unique_paths_iii import unique_paths_iii
+
+
+@pytest.mark.parametrize(
+    "grid, expected",
+    [
+        ([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 2, -1]], 2),
+        ([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 2]], 4),
+        ([[0, 1], [2, 0]], 0),
+    ],
+)
+def test_unique_paths_iii(grid, expected):
+    assert unique_paths_iii(grid) == expected

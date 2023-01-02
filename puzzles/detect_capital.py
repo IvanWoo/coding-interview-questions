a9412ebd30f5e04c571b3c86e4e94098360694fb
Yifan Wu <tyivanwu@gmail.com>
2023-01-01 19:39:54 -0500

    feat: add detect_capital

---
+++ puzzles/detect_capital.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/detect-capital/description/
+"""
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+Example 1:
+Input: word = "USA"
+Output: true
+
+Example 2:
+Input: word = "FlaG"
+Output: false
+ 
+
+Constraints:
+1 <= word.length <= 100
+word consists of lowercase and uppercase English letters.
+"""
+
+
+def detect_capital_use(word: str) -> bool:
+    def all_lower(w):
+        return all([ord(c) >= ord("a") for c in w])
+
+    def all_upper(w):
+        return all([ord(c) < ord("a") for c in w])
+
+    is_lower = ord(word[0]) >= ord("a")
+    if is_lower:
+        return all_lower(word[1:])
+    else:
+        return all_lower(word[1:]) or all_upper(word[1:])
+
+
+def detect_capital_use(word: str) -> bool:
+    return word.isupper() or word.islower() or word.istitle()
---
+++ tests/test_detect_capital.py
@@ -0,0 +1,17 @@
+import pytest
+from puzzles.detect_capital import detect_capital_use
+
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("USA", True),
+        ("FlaG", False),
+        ("leetcode", True),
+        ("A", True),
+        ("Aa", True),
+        ("mL", False),
+    ],
+)
+def test_detect_capital_use(word, expected):
+    assert detect_capital_use(word) == expected

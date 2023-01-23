8d107e17283380bb6c9b44623a73e5d9ac3f1953
Yifan Wu <tyivanwu@gmail.com>
2023-01-22 21:17:34 -0500

    feat: add find_the_town_judge

---
+++ tests/test_find_the_town_judge.py
@@ -0,0 +1,14 @@
+import pytest
+from puzzles.find_the_town_judge import find_judge
+
+
+@pytest.mark.parametrize(
+    "n, trusted, expected",
+    [
+        (2, [[1, 2]], 2),
+        (3, [[1, 3], [2, 3]], 3),
+        (3, [[1, 3], [2, 3], [3, 1]], -1),
+    ],
+)
+def test_find_judge(n, trusted, expected):
+    assert find_judge(n, trusted) == expected

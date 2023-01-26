81c1aab94d462bce905168c64e1c29dd63ec4a8b
Yifan Wu <tyivanwu@gmail.com>
2023-01-25 20:42:55 -0500

    refactor: revisit cheapest_flights_within_k_stops

--- puzzles/cheapest_flights_within_k_stops.py
+++ puzzles/cheapest_flights_within_k_stops.py
@@ -6,7 +6,7 @@ Now given all the cities and flights, together with starting city src and the de
 
 Example 1:
 Input: 
-n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]]
 src = 0, dst = 2, k = 1
 Output: 200
 Explanation: 
@@ -15,7 +15,7 @@ The cheapest price from city 0 to city 2 with at most 1 stop costs 200, as marke
 
 Example 2:
 Input: 
-n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]]
 src = 0, dst = 2, k = 0
 Output: 500
 Explanation: 
@@ -30,59 +30,69 @@ The price of each flight will be in the range [1, 10000].
 k is in the range of [0, n - 1].
 There will not be any duplicated flights or self cycles.
 """
-from typing import List
 import heapq
+from collections import deque
+from math import inf
 
 
-# bfs
+# bfs: TLE
 def find_cheapest_price(
-    n: int, edges: List[List[int]], src: int, dst: int, k: int
+    n: int, flights: list[list[int]], src: int, dst: int, k: int
 ) -> int:
-    graph = [[float("inf")] * n for _ in range(n)]
-    for u, v, d in edges:
+    graph = [[inf] * n for _ in range(n)]
+    for u, v, d in flights:
         graph[u][v] = d
 
-    ans = float("inf")
-    q = set([(src, 0, 0)])
+    ans = inf
+    visited = set()
+    q = deque([(src, k, 0)])
     while q:
-        new_q = set()
-        while q:
-            pos, cost, stop = q.pop()
-            if stop > k or cost >= ans:
+        cur, stop, cost = q.popleft()
+        if stop < -1:
+            continue
+        if cur == dst:
+            ans = min(ans, cost)
+            continue
+        if cur in visited and cost > ans:
+            continue
+        visited.add(cur)
+        for nxt in range(n):
+            if nxt == cur:
                 continue
-            for new_pos in range(n):
-                if new_pos == dst:
-                    ans = min(ans, cost + graph[pos][new_pos])
-                    continue
-                new_q.add((new_pos, cost + graph[pos][new_pos], stop + 1))
-        q = new_q
-    return ans if ans != float("inf") else -1
+            nxt_cost = graph[cur][nxt]
+            q.append((nxt, stop - 1, cost + nxt_cost))
+    return ans if ans != inf else -1
 
 
+# priority queue
 def find_cheapest_price(
-    n: int, edges: List[List[int]], src: int, dst: int, k: int
+    n: int, flights: list[list[int]], src: int, dst: int, k: int
 ) -> int:
-    graph = [[float("inf")] * n for _ in range(n)]
-    for u, v, d in edges:
+    graph = [[inf] * n for _ in range(n)]
+    for u, v, d in flights:
         graph[u][v] = d
 
+    visited = set()
     pq = [(0, src, k + 1)]
     while pq:
-        cost, pos, left_stop = heapq.heappop(pq)
-        if cost == float("inf"):
+        cost, cur, stop = heapq.heappop(pq)
+        if cost == inf:
             continue
-        if pos == dst:
+        if cur == dst:
             return cost
-        if left_stop > 0:
-            for new_pos in range(n):
-                heapq.heappush(pq, (cost + graph[pos][new_pos], new_pos, left_stop - 1))
+        if (cur, stop) in visited:
+            continue
+        visited.add((cur, stop))
+        if stop > 0:
+            for nxt in range(n):
+                heapq.heappush(pq, (cost + graph[cur][nxt], nxt, stop - 1))
     return -1
 
 
 if __name__ == "__main__":
     find_cheapest_price(
         n=4,
-        edges=[[0, 1, 1], [0, 2, 5], [1, 2, 1], [2, 3, 1]],
+        flights=[[0, 1, 1], [0, 2, 5], [1, 2, 1], [2, 3, 1]],
         src=0,
         dst=3,
         k=1,
--- tests/test_cheapest_flights_within_k_stops.py
+++ tests/test_cheapest_flights_within_k_stops.py
@@ -1,36 +1,22 @@
+import pytest
 from puzzles.cheapest_flights_within_k_stops import find_cheapest_price
 
 
-def test_find_cheapest_price():
-    assert (
-        find_cheapest_price(
-            n=3, edges=[[0, 1, 100], [1, 2, 100], [0, 2, 500]], src=0, dst=2, k=1
-        )
-        == 200
-    )
-    assert (
-        find_cheapest_price(
-            n=3, edges=[[0, 1, 100], [1, 2, 100], [0, 2, 500]], src=0, dst=2, k=0
-        )
-        == 500
-    )
-    assert (
-        find_cheapest_price(
-            n=5,
-            edges=[[4, 1, 1], [1, 2, 3], [0, 3, 2], [0, 4, 10], [3, 1, 1], [1, 4, 3]],
-            src=2,
-            dst=1,
-            k=1,
-        )
-        == -1
-    )
-    assert (
-        find_cheapest_price(
-            n=4,
-            edges=[[0, 1, 1], [0, 2, 5], [1, 2, 1], [2, 3, 1]],
-            src=0,
-            dst=3,
-            k=1,
-        )
-        == 6
-    )
+@pytest.mark.parametrize(
+    "n, flights, src, dst, k, expected",
+    [
+        (3, [[0, 1, 100], [1, 2, 100], [0, 2, 500]], 0, 2, 1, 200),
+        (3, [[0, 1, 100], [1, 2, 100], [0, 2, 500]], 0, 2, 0, 500),
+        (
+            5,
+            [[4, 1, 1], [1, 2, 3], [0, 3, 2], [0, 4, 10], [3, 1, 1], [1, 4, 3]],
+            2,
+            1,
+            1,
+            -1,
+        ),
+        (4, [[0, 1, 1], [0, 2, 5], [1, 2, 1], [2, 3, 1]], 0, 3, 1, 6),
+    ],
+)
+def test_find_cheapest_price(n, flights, src, dst, k, expected):
+    assert find_cheapest_price(n, flights, src, dst, k) == expected

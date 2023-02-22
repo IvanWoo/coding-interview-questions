fcef2510852fb045350395884402fe2d2c922308
Yifan Wu <tyivanwu@gmail.com>
2023-02-21 19:42:38 -0500

    refactor: revisit capacity_to_ship_packages_within_d_days

--- puzzles/capacity_to_ship_packages_within_d_days.py
+++ puzzles/capacity_to_ship_packages_within_d_days.py
@@ -1,13 +1,13 @@
 # https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
 """
-A conveyor belt has packages that must be shipped from one port to another within D days.
+A conveyor belt has packages that must be shipped from one port to another within days days.
 
 The i-th package on the conveyor belt has a weight of weights[i].  Each day, we load the ship with packages on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
 
-Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within D days.
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
 
 Example 1:
-Input: weights = [1,2,3,4,5,6,7,8,9,10], D = 5
+Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
 Output: 15
 Explanation:
 A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
@@ -19,7 +19,7 @@ A ship capacity of 15 is the minimum to ship all the packages in 5 days like thi
 Note that the cargo must be shipped in the order given, so using a ship of capacity 14 and splitting the packages into parts like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) is not allowed.
 
 Example 2:
-Input: weights = [3,2,2,4,1,4], D = 3
+Input: weights = [3,2,2,4,1,4], days = 3
 Output: 6
 Explanation:
 A ship capacity of 6 is the minimum to ship all the packages in 3 days like this:
@@ -28,7 +28,7 @@ A ship capacity of 6 is the minimum to ship all the packages in 3 days like this
 3rd day: 1, 4
 
 Example 3:
-Input: weights = [1,2,3,1,1], D = 4
+Input: weights = [1,2,3,1,1], days = 4
 Output: 3
 Explanation:
 1st day: 1
@@ -38,37 +38,36 @@ Explanation:
 
 Constraints:
 
-1 <= D <= weights.length <= 50000
+1 <= days <= weights.length <= 50000
 1 <= weights[i] <= 500
 """
-from typing import List
 
 
-def count_ds(weights, c):
-    i = 0
-    ans = 1
-    left = c
-    while i < len(weights):
-        if weights[i] > left:
-            ans += 1
-            left = c
-            continue
-        else:
-            left -= weights[i]
-            i += 1
+def ship_within_days(weights: list[int], days: int) -> int:
+    def count_ds(weights, c):
+        i = 0
+        ans = 1
+        left = c
+        while i < len(weights):
+            if weights[i] > left:
+                ans += 1
+                left = c
+                continue
+            else:
+                left -= weights[i]
+                i += 1
 
-    return ans
+        return ans
 
-
-def ship_within_days(weights: List[int], D: int) -> int:
     lo, hi = max(weights), sum(weights)
     while lo <= hi:
         mid = (lo + hi) // 2
-        if count_ds(weights, mid) > D:
+        c = count_ds(weights, mid)
+        if c > days:
             lo = mid + 1
-        elif count_ds(weights, mid) < D:
+        elif c < days:
             hi = mid - 1
-        elif count_ds(weights, mid) == D:
+        elif c == days:
             hi = mid - 1
 
     return lo

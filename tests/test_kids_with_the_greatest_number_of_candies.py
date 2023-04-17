fd531d8a3fb48b023847b24300e38b3e9fd94e14
Yifan Wu <tyivanwu@gmail.com>
2023-04-16 20:06:48 -0400

    feat: add kids_with_the_greatest_number_of_candies

---
+++ tests/test_kids_with_the_greatest_number_of_candies.py
@@ -0,0 +1,15 @@
+import pytest
+
+from puzzles.kids_with_the_greatest_number_of_candies import kids_with_candies
+
+
+@pytest.mark.parametrize(
+    "candies, extra_candies, expected",
+    [
+        ([2, 3, 5, 1, 3], 3, [True, True, True, False, True]),
+        ([4, 2, 1, 1, 2], 1, [True, False, False, False, False]),
+        ([12, 1, 12], 10, [True, False, True]),
+    ],
+)
+def test_kids_with_candies(candies, extra_candies, expected):
+    assert kids_with_candies(candies, extra_candies) == expected

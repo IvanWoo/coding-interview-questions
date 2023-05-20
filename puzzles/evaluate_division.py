3004bb72366d0a078a2bd09511d311dffa2586bc
Yifan Wu <tyivanwu@gmail.com>
2023-05-19 21:36:24 -0400

    refactor: revisit evaluate_division

--- puzzles/evaluate_division.py
+++ puzzles/evaluate_division.py
@@ -91,11 +91,3 @@ def calc_equation(
         return -1
 
     return [bfs(s, d) for s, d in queries]
-
-
-if __name__ == "__main__":
-    calc_equation(
-        equations=[["a", "b"], ["b", "c"]],
-        values=[2.0, 3.0],
-        queries=[["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]],
-    )

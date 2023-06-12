4fc5db22b64b3bcf0a5cc13a2a2210b208808254
Yifan Wu <tyivanwu@gmail.com>
2023-06-11 21:10:01 -0400

    feat: add summary_ranges

---
+++ tests/test_summary_ranges.py
@@ -0,0 +1,14 @@
+import pytest
+
+from puzzles.summary_ranges import summary_ranges
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([0, 1, 2, 4, 5, 7], ["0->2", "4->5", "7"]),
+        ([0, 2, 3, 4, 6, 8, 9], ["0", "2->4", "6", "8->9"]),
+    ],
+)
+def test_summary_ranges(nums, expected):
+    assert summary_ranges(nums) == expected
